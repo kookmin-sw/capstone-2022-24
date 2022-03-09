ba83e784fdbffed44c6b4e075c8bb9f8e84b8b79
asung <a.city521@gmail.com>
2022-03-09 15:18:26 +0900

    design: gnb 페이지 전역 등록

--- src/front-end/src/components/GNB.vue
+++ src/front-end/src/components/GNB.vue
@@ -12,19 +12,20 @@
 				</div>
 			</div>
 			<div class="icon-menu-frame col-2">
-				<div class="row">
-					<q-space class="col-3" />
-					<div class="col-4">
-						<q-icon name="person" size="md" color="blue" />
-					</div>
-					<div class="col-4">
-						<q-icon name="notifications_none" size="md" color="blue" />
-					</div>
-					<q-space class="col-1" />
-				</div>
+				<!--				<div class="row">-->
+				<!--					<q-space class="col-3" />-->
+				<!--					<div class="col-4">-->
+				<!--						<q-icon name="person" size="md" color="blue" />-->
+				<!--					</div>-->
+				<!--					<div class="col-4">-->
+				<!--						<q-icon name="notifications_none" size="md" color="blue" />-->
+				<!--					</div>-->
+				<!--					<q-space class="col-1" />-->
+				<!--				</div>-->
 			</div>
 			<div class="col-2">
 				<q-btn unelevated color="blue">로그인/회원가입</q-btn>
+				<!--				<q-btn unelevated color="blue">로그아웃</q-btn>-->
 			</div>
 		</div>
 		<q-separator color="blue" inset />
