110307b836875d206332584ee74e0bfff4107ad8
asung <a.city521@gmail.com>
2022-03-09 15:07:01 +0900

    design: gnb 상단 고정

--- src/front-end/src/components/GNB.vue
+++ src/front-end/src/components/GNB.vue
@@ -1,29 +1,33 @@
 <template>
-	<div class="gnb row">
-		<div class="col-2">
-			<span> logo </span>
-		</div>
-		<div class="text-menu-frame col-6">
-			<div class="row">
-				<span class="col-4"> 홈 </span>
-				<span class="col-4"> 종료 예정작 </span>
-				<span class="col-4"> 모임 신청 </span>
+	<div class="gnb container">
+		<div class="row">
+			<div class="col-2">
+				<span> logo </span>
 			</div>
-		</div>
-		<div class="icon-menu-frame col-2">
-			<div class="row">
-				<q-space class="col-4" />
-				<div class="col-4">
-					<q-icon name="person" size="md" color="blue" />
-				</div>
-				<div class="col-4">
-					<q-icon name="notifications_none" size="md" color="blue" />
+			<div class="text-menu-frame col-6">
+				<div class="row">
+					<span class="col-4"> 홈 </span>
+					<span class="col-4"> 종료 예정작 </span>
+					<span class="col-4"> 모임 신청 </span>
 				</div>
 			</div>
+			<div class="icon-menu-frame col-2">
+				<div class="row">
+					<q-space class="col-3" />
+					<div class="col-4">
+						<q-icon name="person" size="md" color="blue" />
+					</div>
+					<div class="col-4">
+						<q-icon name="notifications_none" size="md" color="blue" />
+					</div>
+					<q-space class="col-1" />
+				</div>
+			</div>
+			<div class="col-2">
+				<q-btn unelevated color="blue">로그인/회원가입</q-btn>
+			</div>
 		</div>
-		<div class="col-2">
-			<q-btn unelevated color="primary">로그인/회원가입</q-btn>
-		</div>
+		<q-separator color="blue" inset />
 	</div>
 </template>
 
@@ -35,8 +39,11 @@ export default {
 
 <style scoped>
 .gnb {
+	position: fixed;
+	top: 0;
+	width: 100%;
 	height: 84px;
 	line-height: 84px;
-	/*background-color: tomato;*/
+	background-color: white;
 }
 </style>
