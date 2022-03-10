f7e619a8f5d1768907f184af8985d51bb94c5104
asung <a.city521@gmail.com>
2022-03-10 15:36:27 +0900

    design: footer 텍스트 메뉴 재정렬

--- src/front-end/src/components/ServiceFooter.vue
+++ src/front-end/src/components/ServiceFooter.vue
@@ -1,20 +1,19 @@
 <template>
 	<!--  <q-separator color="blue" inset />-->
 	<div class="footer">
-		<div class="text-menu-frame text-left row">
-			<div class="col">회사소개</div>
-			<div class="col">이용약관</div>
-			<div class="col">개인정보처리방침</div>
-			<div class="col">고객센터</div>
-			<div class="col">1:1문의</div>
-			<div class="col-4"></div>
+		<div class="text-menu-frame row text-left text-grey-8">
+			<div>회사소개</div>
+			<div>이용약관</div>
+			<div>개인정보처리방침</div>
+			<div>고객센터</div>
+			<div>1:1문의</div>
 		</div>
 		<div class="business-info col">
-			<div class="row text-left">
+			<div class="row text-left text-grey">
 				(주) 서비스명 | 대표이사: 구다희 | 소재지: 서울 특별시 성북구 정릉로 77
 				| 사업자 등록번호: 123-45-6790
 			</div>
-			<div class="row text-left">copyright 어쩌구</div>
+			<div class="row text-left text-grey">copyright 어쩌구</div>
 		</div>
 	</div>
 </template>
@@ -32,8 +31,11 @@ export default {
 	/*background-color:yellowgreen;*/
 }
 
+.text-menu-frame div {
+	margin-right: 12px;
+}
+
 .business-info {
 	margin-top: 16px;
-	color: #bdbdbd;
 }
 </style>
