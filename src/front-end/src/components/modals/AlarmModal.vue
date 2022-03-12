8dff378e6e344b3e237154ea93b65e09fd9248e8
asung <a.city521@gmail.com>
2022-03-12 21:47:40 +0900

    design: 알림 모달 디자인

---
+++ src/front-end/src/components/modals/AlarmModal.vue
@@ -0,0 +1,67 @@
+<template>
+	<q-card style="width: 480px; max-width: 60vw" v-if="isActive">
+		<!--    alarm title    -->
+		<q-card-section class="row items-center q-pb-none q-pt-sm">
+			<div class="text-subtitle1 text-weight-bold">
+				<q-icon name="notifications_none" size="md" color="blue" />
+				{{ $t('alarm') }}
+			</div>
+			<q-space />
+			<q-btn icon="close" flat round dense v-close-popup />
+		</q-card-section>
+
+		<!--    delete button   -->
+		<q-card-section class="col text-right q-ma-none q-mr-lg" v-if="isActive">
+			<q-btn flat>전체 읽음 표시</q-btn>
+			<q-btn flat>읽은 알림 삭제</q-btn>
+			<q-btn flat>전체 삭제</q-btn>
+		</q-card-section>
+		<q-separator color="blue" />
+
+		<!--    msg 1   -->
+		<q-separator color="blue" />
+		<q-card-section
+			class="row text-center q-mt-md q-mr-lg q-mb-sm q-ml-lg"
+			v-if="isActive">
+			<div class="text-left col-11">
+				<span class="q-mr-sm text-weight-bold">모임 알림</span>
+				<span>구독 종료까지 3일 남았습니다!</span>
+				<div class="text-grey-8">넷플릭스 모임 구독 종료일 - 6월 30일</div>
+				<q-space />
+				<div class="text-right q-mt-sm text-grey">6월 28일</div>
+			</div>
+			<div class="col-1">
+				<q-btn icon="close" flat round dense color="blue" size="sm" />
+			</div>
+		</q-card-section>
+		<!--    msg 2   -->
+		<q-separator color="blue" />
+		<q-card-section
+			class="row text-center q-mt-md q-mr-lg q-mb-sm q-ml-lg"
+			v-if="isActive">
+			<div class="text-left col-11">
+				<span class="q-mr-sm text-weight-bold">결제 알림</span>
+				<span>왓챠 구독 후 계정 정보를 등록해주세요!</span>
+				<div class="text-grey-8">왓챠 모임 결제 요청</div>
+				<q-space />
+				<div class="text-right q-mt-sm text-grey">5월 31일</div>
+			</div>
+			<div class="col-1">
+				<q-btn icon="close" flat round dense color="blue" size="sm" />
+			</div>
+		</q-card-section>
+	</q-card>
+</template>
+<script>
+export default {
+	name: 'AlarmModal',
+	props: {
+		isActive: {
+			type: Boolean,
+			require: true,
+		},
+	},
+};
+</script>
+
+<style scoped></style>
