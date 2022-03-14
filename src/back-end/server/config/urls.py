29044229f74789a476993ad50cf3a5978607c6b4
Dahee Goo <heeouo@kookmin.ac.kr>
2022-03-15 01:11:55 +0900

    chore: 앱 13종 추가, 백엔드 디렉토리 구조 변경

--- src/back-end/server/config/urls.py
+++ src/back-end/server/config/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('videos/', include('videos.urls')),
+    path('groups/', include('groups.urls')),
+    path('users/', include('users.urls')),
 ]
---
+++ src/back-end/server/recent_views/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecentViewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recent_views'
