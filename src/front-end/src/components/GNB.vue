a69ddddf39e08e5866ccfc0a47ddb19fb73f6e58
asung <a.city521@gmail.com>
2022-03-16 22:32:41 +0900

    feat: gnb 텍스트 메뉴 라우팅

--- src/front-end/src/components/GNB.vue
+++ src/front-end/src/components/GNB.vue
@@ -1,71 +1,74 @@
 <template>
-	<!--  Login Modal -->
-	<q-dialog v-model="isLoginModal">
-		<login-modal :isActive="isLoginModal"></login-modal>
-	</q-dialog>
-	<!--  GNB -->
-	<div class="gnb container">
-		<div class="row items-center">
-			<div class="col-2">
-				<span class="text-h6 text-weight-bolder">
-					{{ $t('serviceName') }}
-				</span>
-			</div>
-			<div class="text-menu-frame col-6">
-				<div class="row">
-					<q-btn flat class="col-4"> {{ $t('gnb.home') }} </q-btn>
-					<q-btn flat class="col-4"> {{ $t('gnb.discontinued') }} </q-btn>
-					<q-btn flat class="col-4"> {{ $t('gnb.groupJoin') }} </q-btn>
-				</div>
-			</div>
-			<div class="icon-menu-frame col-2">
-				<div class="row">
-					<q-space class="col-3" />
-					<q-btn flat class="col-4">
-						<q-icon name="person" size="md" color="blue" />
-					</q-btn>
-					<q-btn flat class="col-4">
-						<q-icon name="notifications_none" size="md" color="blue" />
-					</q-btn>
-					<q-space class="col-1" />
-				</div>
-			</div>
-			<div class="col-2">
-				<q-btn unelevated color="blue" @click="loginBtnClick">
-					{{ $t('gnb.login') }}
-				</q-btn>
-				<!--				<q-btn unelevated color="blue">{{ $t('gnb.logout') }}</q-btn>-->
-			</div>
-		</div>
-		<q-separator color="blue" inset />
-	</div>
+  <!--  Login Modal -->
+  <q-dialog v-model="isLoginModal">
+    <login-modal :isActive="isLoginModal"></login-modal>
+  </q-dialog>
+  <!--  GNB -->
+  <div class="gnb container">
+    <div class="row items-center">
+      <div class="col-2">
+        <span class="text-h6 text-weight-bolder">
+          {{ $t('serviceName') }}
+        </span>
+      </div>
+      <div class="text-menu-frame col-6 row">
+        <router-link to="/" class="col-4">
+          {{ $t('gnb.home') }}
+        </router-link>
+        <router-link to="/discontinue" class="col-4">
+          {{ $t('gnb.discontinued') }}
+        </router-link>
+        <router-link to="/join" class="col-4">
+          {{ $t('gnb.groupJoin') }}
+        </router-link>
+      </div>
+      <div class="icon-menu-frame col-2">
+        <div class="row">
+          <q-space class="col-3" />
+          <q-btn flat class="col-4">
+            <q-icon name="person" size="md" color="blue" />
+          </q-btn>
+          <q-btn flat class="col-4">
+            <q-icon name="notifications_none" size="md" color="blue" />
+          </q-btn>
+          <q-space class="col-1" />
+        </div>
+      </div>
+      <div class="col-2">
+        <q-btn unelevated color="blue" @click="loginBtnClick">
+          {{ $t('gnb.login') }}
+        </q-btn>
+        <!--				<q-btn unelevated color="blue">{{ $t('gnb.logout') }}</q-btn>-->
+      </div>
+    </div>
+    <q-separator color="blue" inset />
+  </div>
 </template>
 
 <script>
 import LoginModal from '@/components/modals/LoginModal';
 export default {
-	name: 'GNB',
-	components: {
-		LoginModal,
-	},
-	data() {
-		return {
-			isLoginModal: false,
-		};
-	},
-	methods: {
-		loginBtnClick() {
-			this.isLoginModal = !this.isLoginModal;
-		},
-	},
+  name: 'GNB',
+  components: {
+    LoginModal,
+  },
+  data() {
+    return {
+      isLoginModal: false,
+    };
+  },
+  methods: {
+    loginBtnClick() {
+      this.isLoginModal = !this.isLoginModal;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .gnb {
-	height: 84px;
-	line-height: 84px;
-	background-color: white;
-	z-index: 10;
+  height: 84px;
+  line-height: 84px;
+  background-color: white;
 }
 </style>
