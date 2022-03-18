fbbea037716306fda3af1d77adf497737266e4bb
asung <a.city521@gmail.com>
2022-03-18 12:57:11 +0900

    chore: about 페이지 삭제

--- src/front-end/src/router/index.js
+++ src/front-end/src/router/index.js
@@ -2,43 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/home',
-    redirect: '/',
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-  },
-  {
-    path: '/join',
-    name: 'Join',
-    component: () => import(/* webpackChunkName: "Join" */ '@/views/Join.vue'),
-  },
-  {
-    path: '/discontinue',
-    name: 'Discontinue',
-    component: () =>
-      import(/* webpackChunkName: "Join" */ '@/views/Discontinue.vue'),
-  },
-  {
-    path: '/details',
-    name: 'Details',
-    component: () =>
-      import(/* webpackChunkName: "Details" */ '@/views/Details.vue'),
-  },
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/home',
+		redirect: '/',
+	},
+	{
+		path: '/join',
+		name: 'Join',
+		component: () => import(/* webpackChunkName: "Join" */ '@/views/Join.vue'),
+	},
+	{
+		path: '/discontinue',
+		name: 'Discontinue',
+		component: () =>
+			import(/* webpackChunkName: "Join" */ '@/views/Discontinue.vue'),
+	},
+	{
+		path: '/details',
+		name: 'Details',
+		component: () =>
+			import(/* webpackChunkName: "Details" */ '@/views/Details.vue'),
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
 
 export default router;
