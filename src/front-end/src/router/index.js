59eb454e93a50f14b9e38507378d93e0e54bf1bf
asung <a.city521@gmail.com>
2022-03-18 14:40:00 +0900

    feat: 모임 신청 페이지 네비게이션 가드

--- src/front-end/src/router/index.js
+++ src/front-end/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 
+import store from '@/store/index.js';
+
 const routes = [
 	{
 		path: '/',
@@ -15,6 +17,14 @@ const routes = [
 		path: '/join',
 		name: 'Join',
 		component: () => import(/* webpackChunkName: "Join" */ '@/views/Join.vue'),
+		beforeEnter: (to, from, next) => {
+			if (store.getters['auth/isLogin'] === false) {
+				alert('로그인 후 사용하실 수 있습니다.');
+				next('/');
+			} else {
+				next();
+			}
+		},
 	},
 	{
 		path: '/discontinue',
@@ -27,6 +37,52 @@ const routes = [
 		name: 'Details',
 		component: () =>
 			import(/* webpackChunkName: "Details" */ '@/views/Details.vue'),
+	},
+	{
+		path: '/introduction',
+		name: 'Introduction',
+		component: () =>
+			import(
+				/* webpackChunkName: "Introduction" */ '@/views/footer/Introduction.vue'
+			),
+	},
+	{
+		path: '/tos',
+		name: 'TermsOfService',
+		component: () =>
+			import(/* webpackChunkName: "Tos" */ '@/views/footer/Tos.vue'),
+	},
+	{
+		path: '/privacy',
+		name: 'PrivacyPolicy',
+		component: () =>
+			import(
+				/* webpackChunkName: "Privacy" */ '@/views/footer/PrivacyPolicy.vue'
+			),
+	},
+	{
+		path: '/center',
+		name: 'Center',
+		component: () =>
+			import(/* webpackChunkName: "Center" */ '@/views/footer/Center.vue'),
+	},
+	{
+		path: '/questions',
+		name: 'Questions',
+		component: () =>
+			import(
+				/* webpackChunkName: "Questions" */ '@/views/footer/Questions.vue'
+			),
+	},
+	{
+		path: '/404',
+		name: 'NotFound',
+		component: () =>
+			import(/* webpackChunkName: "NotFound" */ '@/views/NotFound.vue'),
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		redirect: '/404',
 	},
 ];
 
