7b9e2877c8ecd6304a38198359d6610f3e7f1b6f
asung <a.city521@gmail.com>
2022-03-18 23:42:21 +0900

    style: lint auto fix

--- src/front-end/src/views/footer/PrivacyPolicy.vue
+++ src/front-end/src/views/footer/PrivacyPolicy.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="q-ma-lg">개인 정보 처리 방침 페이지 입니다!!</div>
+	<div class="q-ma-lg">개인 정보 처리 방침 페이지 입니다!!</div>
 </template>
 
 <script>
 export default {
-  name: 'PrivacyPolicy',
+	name: 'PrivacyPolicy',
 };
 </script>
 
