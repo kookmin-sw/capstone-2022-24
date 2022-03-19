37d85d9204619017bfe883af670e3df97b9492b6
yesslee <lys0301174@kookmin.ac.kr>
2022-03-19 23:38:15 +0900

    design: 사용자 상호작용 작품 전체보기 페이지 작업 완료

--- src/front-end/src/views/Expand.vue
+++ src/front-end/src/views/Expand.vue
@@ -6,30 +6,24 @@
       <q-btn flat class="q-mt-lg q-mb-lg">찜한 작품</q-btn>
       <q-btn flat class="q-mt-lg q-mb-lg">별점 준 작품</q-btn>
     </div>
-    <div class="row q-ma-lg">
-      <q-card flat bordered>
-        <q-card-section class="q-pa-sm">
-          <div class="poster"></div>
-          <div class="text-left">영화 제목</div>
-          <div class="row no-wrap items-center">
-          <div class="row text-right">
-            <q-rating size="18px" v-model="stars" :max="5" color="blue" />
-            <div>2022.03.14</div>
-          </div>
-          </div>
-        </q-card-section>
-      </q-card>
-    </div>
-
     <div>
       <q-infinite-scroll :offset="250" @load="videoOnLoad">
-        <div class="row q-ma-lg video-list-frame">
-          <div
-              class="video-poster"
-              v-for="(video, index) in videos"
-              :key="index">
-            {{ video.value }}
-          </div>
+        <div class="row justify-center col-gap-16 q-ma-lg">
+          <q-card flat bordered
+                  class="video-list-frame"
+                  v-for="(video, index) in videos"
+                  :key="index">
+            <q-card-section class="q-pa-sm">
+              <div class="video-poster">{{ video.value }}</div>
+              <div class="text-left">영화 제목</div>
+              <div class="row no-wrap items-center q-mt-sm">
+                <div class="col text-right">
+                  <q-rating size="18px" v-model="stars" :max="5" color="blue" />
+                  <div>2022.03.14</div>
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
         </div>
         <template v-slot:loading>
           <div class="row q-mb-lg justify-center">
@@ -82,21 +76,12 @@ export default {
 
 <style scoped>
 .video-list-frame {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  width: 170px;
+  margin-bottom: 16px;
 }
-
 .video-poster {
-  width: 15%;
-  height: 0;
-  padding-bottom: 20%;
-  margin: 0 0 24px 0;
+  width: 100%;
+  height: 210px;
   background: lightgrey;
-}
-
-.badge-frame {
-  display: flex;
-  flex-wrap: wrap;
 }
 </style>
