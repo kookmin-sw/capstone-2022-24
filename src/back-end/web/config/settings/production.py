6627c442b99cd36ee8cc651af464108f0fa0cbf1
Dahee Goo <heeouo@kookmin.ac.kr>
2022-03-22 07:35:03 +0900

    chore: production, development 파일도 추가

---
+++ src/back-end/web/config/settings/production.py
@@ -0,0 +1,45 @@
+"""production settings file"""
+# pylint: disable=R0801,W0401,W0614
+import environ
+
+from .base import *
+
+env = environ.Env(DEBUG=(bool, False))
+
+# reading environment file
+environ.Env.read_env(env_file=os.path.join(ENV_DIR, ".env.prod"))
+
+DEBUG = False
+
+ALLOWED_HOSTS = [env("WEB_HOST"), env("WEB_DNS"), "localhost"]
+
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": env("DB_NAME"),
+        "ENFORCE_SCHEMA": env("DB_ENFORCE_SCHEMA"),
+        'CLIENT': {
+            'host': env('DB_HOST_NAME'),
+            'port': int(env('DB_PORT')),
+            'username': env('DB_USER'),
+            'password': env('DB_PASSWORD'),
+            'authSource': 'admin',
+            'authMechanism': 'SCRAM-SHA-1',
+        },
+        "LOGGING": {
+            "version": 1,
+            "loggers": {
+                "djongo": {
+                    "level": "DEBUG",
+                    "propagate": False,
+                }
+            },
+        },
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
