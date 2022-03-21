13c712b193e145b5cc656b1ca3c44846c7f1328d
Dahee Goo <heeouo@kookmin.ac.kr>
2022-03-21 16:38:42 +0900

    chore: base.py 파일 경로 다시 이동, BASE_DIR 수정

--- src/back-end/web/config/settings/base.py
+++ src/back-end/web/config/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
 
 
 # Default primary key field type
