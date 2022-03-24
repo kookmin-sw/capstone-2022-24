17bee5bf89e7c4210b300755ba1f2fa5efb6a2b8
asung <a.city521@gmail.com>
2022-03-25 00:29:05 +0900

    chore: 이미지 input field 주석 처리

--- src/front-end/src/views/Register.vue
+++ src/front-end/src/views/Register.vue
@@ -2,15 +2,13 @@
 	<div class="Register q-ma-lg">
 		<div class="text-h6 text-weight-bold">회원가입</div>
 		<div>사용할 프로필 사진과 닉네임을 입력해주세요.</div>
-		<q-input
-			@update:model-value="
-				val => {
-					file = val[0];
-				}
-			"
-			filled
-			type="file"
-			class="q-mb-md" />
+		<div>
+			<q-avatar
+				rounded
+				size="100px"
+				:style="`background-image : url(${selectImg})`" />
+		</div>
+		<!--		<input @change="uploadExample" accept="image/*" type="file" id="file" />-->
 		<div class="row q-mb-lg">
 			<q-input dense label="닉네임" class="col-8 q-pr-lg" v-model="nickname" />
 			<q-btn
@@ -36,6 +34,7 @@ export default {
 		return {
 			nickname: null,
 			canNickname: false,
+			selectImg: '',
 		};
 	},
 	methods: {
@@ -55,6 +54,11 @@ export default {
 					this.canNickname = false;
 				});
 		},
+		// uploadExample(e) {
+		// 	var file = e.target.files;
+		// 	let url = URL.createObjectURL(file[0]); // file[0].type과 같이 js에서 .type 사용하면 image/png 등 확장자 검사가 가능
+		// 	this.selectImg = url;
+		// },
 	},
 };
 </script>
