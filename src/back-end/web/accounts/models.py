0755a2f3bc58512766f85bc2c07d40090e6af257
Dahee Goo <heeouo@kookmin.ac.kr>
2022-03-25 05:59:09 +0900

    feat: Bank, Account 모델 및 시리얼라이저 작성

--- src/back-end/web/accounts/models.py
+++ src/back-end/web/accounts/models.py
@@ -1,3 +1,26 @@
 from djongo import models
 
-# Create your models here.
+
+class Bank(models.Model):
+    _id = models.ObjectIdField()
+    code = models.CharField(
+        max_length=10,
+        null=False
+    )
+    name = models.CharField(
+        max_length=10,
+        null=False
+    )
+
+
+class Account(models.Model):
+    _id = models.ObjectIdField()
+    bank = models.ForeignKey(
+        Bank,
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    name = models.CharField(
+        max_length=10,
+        null=False
+    )
