223140c97c4335f92f42ebe34fe757967ba00c8e
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-03-25 13:34:45 +0900

    feat: videos_details Serializer 추가

--- src/back-end/web/videos/serializer.py
+++ src/back-end/web/videos/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import videos
+from .models import videos, video_details
 
 class videosSerialaizer(serializers.Serializer):
     # 시리얼라이즈/디시리얼라이즈 되어야 하는 모델 필드들
@@ -30,4 +30,29 @@ class videosSerialaizer(serializers.Serializer):
     class Meta:
         model = videos
         fields = ['tmdbid', 'title', 'code', 'releaseDate', 'filmRating', 'category', 'posterkey', 'titleEnglish']
+
+class videosDetailsSerialaizer(serializers.Serializer):
+    # 시리얼라이즈/디시리얼라이즈 되어야 하는 모델 필드들
+    runtime = serializers.IntegerField(null=False)
+    releaseDate= serializers.DateField()
+    rating = serializers.ListField(null=False)
+    productionCountry= serializers.CharField(max_length=100, null=False)
+    gernes= serializers.ListField(max_length=50, null=True)
+
+    # Serializer.save()를 통해 DB 인스턴스를 생성할 때의 동작 정의
+    # 유효성 검사를 통과한 데이터들을 바탕으로 새로운 DB 인스턴스를 생성하고 반환
+    def create(self, validated_data):
+        return video_details.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.runtime = validated_data.get('tmdbid', instance.runtime)
+        instance.releaseDate = validated_data.get('title', instance.releaseDate)
+        instance.rating = validated_data.get('releaseDate', instance.rating)
+        instance.productionCountry = validated_data.get('filmRating', instance.productionCountry)
+        instance.gernes= validated_data.get('category', instance.gernes)
+        return instance
+
+    class Meta:
+        model = video_details
+        fields = ['runtime', 'releaseDate', 'rating', 'productionCountry', 'gernes',]
 
