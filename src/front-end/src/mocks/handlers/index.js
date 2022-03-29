154462b11afc9f4fa84d75e8e9a65648c8e075f9
asung <a.city521@gmail.com>
2022-03-29 17:56:12 +0900

    chore: 마이페이지 최초 접근, 최근 조회작 조회 mock data

--- src/front-end/src/mocks/handlers/index.js
+++ src/front-end/src/mocks/handlers/index.js
@@ -1,4 +1,9 @@
 import oauth from '@/mocks/handlers/oauth';
 import register from '@/mocks/handlers/register';
+import user from '@/mocks/handlers/user';
 
-export const handlers = [...Object.values(oauth), ...Object.values(register)];
+export const handlers = [
+	...Object.values(oauth),
+	...Object.values(register),
+	...Object.values(user),
+];
