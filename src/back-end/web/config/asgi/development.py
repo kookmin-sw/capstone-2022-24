7baf92a8c10a9d392c7cd29870cc48d2a4a3d63a
Dahee Goo <heeouo@kookmin.ac.kr>
2022-03-29 16:31:37 +0900

    fix: uvicorn application 수정

--- src/back-end/web/config/asgi/development.py
+++ src/back-end/web/config/asgi/development.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 """Run administrative tasks."""
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
-application = get_wsgi_application()
+application = get_asgi_application()
