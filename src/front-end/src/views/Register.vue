db4f40688094631fbf88236c4eaac6eee7a7a3c3
asung <a.city521@gmail.com>
2022-04-07 21:49:34 +0900

    feat: 회원가입 이미지 유효성 확인

--- src/front-end/src/views/Register.vue
+++ src/front-end/src/views/Register.vue
@@ -1,14 +1,31 @@
 <template>
 	<div class="Register q-ma-lg">
-		<div class="text-h6 text-weight-bold">회원가입</div>
-		<div>사용할 프로필 사진과 닉네임을 입력해주세요.</div>
-		<div>
-			<q-avatar
-				rounded
-				size="100px"
-				:style="`background-image : url(${selectImg})`" />
+		<!-- 회원가입 타이틀 -->
+		<div class="q-mb-lg">
+			<div class="text-h6 text-weight-bold">회원가입</div>
+			<div>사용할 프로필 사진과 닉네임을 입력해주세요.</div>
 		</div>
-		<!--		<input @change="uploadExample" accept="image/*" type="file" id="file" />-->
+
+		<!-- 프로필 사진 입력 -->
+		<q-avatar
+			rounded
+			size="120px"
+			style="border: dotted 2px cornflowerblue"
+			:style="`background-image : url(${selectImg})`">
+			<div @click="clickInputField()">
+				<input
+					ref="image"
+					id="in"
+					type="file"
+					name="image"
+					accept="image/png, image/jpeg, image/jpg"
+					class="hidden"
+					@change="uploadImg()" />
+				<q-icon name="add_circle_outline" />
+			</div>
+		</q-avatar>
+
+		<!-- 닉네임 입력 -->
 		<div class="row q-mb-lg">
 			<q-input dense label="닉네임" class="col-8 q-pr-lg" v-model="nickname" />
 			<q-btn
@@ -19,6 +36,8 @@
 				중복 확인
 			</q-btn>
 		</div>
+
+		<!-- 회원가입 버튼 -->
 		<div>
 			<q-btn unelevated color="blue" class="full-width">회원가입</q-btn>
 		</div>
@@ -54,11 +73,17 @@ export default {
 					this.canNickname = false;
 				});
 		},
-		// uploadExample(e) {
-		// 	var file = e.target.files;
-		// 	let url = URL.createObjectURL(file[0]); // file[0].type과 같이 js에서 .type 사용하면 image/png 등 확장자 검사가 가능
-		// 	this.selectImg = url;
-		// },
+		uploadImg() {
+			const file = this.$refs['image'].files[0];
+			if (file.size > 4194304) {
+				alert('크기가 4MB 이하인 이미지를 선택해주세요.');
+				return;
+			}
+			this.selectImg = URL.createObjectURL(file);
+		},
+		clickInputField() {
+			this.$refs['image'].click();
+		},
 	},
 };
 </script>
