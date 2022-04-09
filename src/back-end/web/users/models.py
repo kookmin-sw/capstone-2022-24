6dfc64e9e9a42aca4ded8ae34dfd05c440876827
Dahee Goo <heeouo@kookmin.ac.kr>
2022-04-10 00:39:16 +0900

    chore: abstractBaseModel 상속받도록 변경

--- src/back-end/web/users/models.py
+++ src/back-end/web/users/models.py
@@ -1,59 +1,81 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils import timezone
 from djongo import models
-# from accounts import model
+from ..accounts.models import Account
 
 
-class social_types(models.Model):
-    _id = models.ObjectIdField()
+class SocialType(models.Model):
+    id = models.BigAutoField(
+        primary_key=True
+    )
     name = models.CharField(
         max_length=10,
-        null=False
-    ),
-    logoKey = models.CharField(
-        max_length=30,
         null=False,
+        blank=False,
+    ),
+    logo_key = models.CharField(
+        null=False,
+        blank=False,
+        max_length=100,
+        db_column="logoKey"
     )
 
+    class Meta:
+        db_table = "social_types"
 
-class users(models.Model):
-    _id = models.ObjectIdField()
+    def __str__(self):
+        return f"{self.name}"
+
+
+class User(AbstractBaseUser, models.Model):
+    id = models.BigAutoField(
+        primary_key=True
+    )
     nickname = models.CharField(
-        max_length=15,
-        null=False
+        max_length=8,
+        null=False,
+        unique=True
     ),
     email = models.EmailField(
         max_length=50,
         null=False
-    )
-    cellPhoneNumber = models.CharField(
+    ),
+    cell_phone_number = models.CharField(
         max_length=14,
         null=False,
+        db_column="cellPhoneNumber"
     ),
-    # account = models.ForeignKey(
-    #     Account,
-    #     null=True,
-    #     on_delete=models.CASCADE
-    # ),
-    socialType = models.EmbeddedField(
+    account = models.ForeignKey(
+        Account,
+        null=True,
+        on_delete=models.CASCADE
+    ),
+    social_type = models.EmbeddedField(
         null=False,
-        model_container=social_types,
+        model_container=SocialType,
     ),
-    profileImageUrl = models.ImageField(
+    profile_image_url = models.ImageField(
         blank=True,
         null=True,
     ),
     birthday = models.DateField(
         null=True
     ),
-    isBlocked = models.BooleanField(
+    is_blocked = models.BooleanField(
         null=False,
         default=False,
     ),
-    registrationDateTime = models.DateTimeField(
+    registration_date_time = models.DateTimeField(
         null=False,
         default=timezone.now,
     ),
-    withdrawalDateTime = models.DateTimeField(
+    withdrawal_date_time = models.DateTimeField(
         null=True,
         default=None,
     )
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return f"{self.nickname}"
