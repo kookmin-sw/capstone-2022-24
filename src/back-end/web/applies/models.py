ecd8c40810215059cad399865e3d90db6c5ba2f9
Dahee Goo <heeouo@kookmin.ac.kr>
2022-04-13 23:56:33 +0900

    feat: Apply 상속 테이블 생성

--- src/back-end/web/applies/models.py
+++ src/back-end/web/applies/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
+from django.utils import timezone
 from djongo import models
+from ..payments.models import Payment
+from ..providers.models import Provider
 
-# Create your models here.
+
+class Apply(models.Model):
+    id = models.BigAutoField(
+        primary_key=True
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=False,
+        on_delete=models.CASCADE,
+        db_column="userId"
+    )
+    payment = models.ForeignKey(
+        Payment,
+        null=False,
+        on_delete=models.CASCADE,
+        db_column="paymentId"
+    )
+    provider = models.ForeignKey(
+        Provider,
+        on_delete=models.CASCADE,
+        db_column="providerId"
+    )
+    apply_date_time = models.DateTimeField(
+        default=timezone.now,
+        db_column="applyDateTime"
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"[{self.provider.name}] {self.user.name}님의 구성원 신청"
