f840c33ed2440fd3234f86b857651d13e1ea47c7
Dahee Goo <heeouo@kookmin.ac.kr>
2022-04-14 04:25:15 +0900

    feat: OTT계정 앱, 모델, 시리얼라이저, 마이그레이션 파일 추가

---
+++ src/back-end/web/group_accounts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+
+class GroupAccount(models.Model):
+    id = models.BigAutoField(
+        primary_key=True
+    )
+    identifier = models.CharField(
+        max_length=50,
+        null=True,
+        blank=False
+    )
+    password = models.CharField(
+        max_length=30,
+        null=True,
+        blank=False
+    )
+    creation_date_time = models.DateTimeField(
+        null=True,
+        blank=False,
+        default=timezone.now,
+        db_column="creationDateTime"
+    )
+    last_modification_date_time = models.DateTimeField(
+        null=True,
+        blank=False,
+        default=timezone.now,
+        db_column="lastModificationDateTime"
+    )
+
+    class Meta:
+        db_table = "group_accounts"
+
+    def __str__(self):
+        return f"[{self.id} 최종 변경일시: {self.last_modification_date_time}"
