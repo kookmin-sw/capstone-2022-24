592c0b5367cbd30367613717190d4af300c3996c
Dahee Goo <heeouo@kookmin.ac.kr>
2022-04-15 05:05:31 +0900

    chore: docstring 추가

--- src/back-end/web/fellows/admin.py
+++ src/back-end/web/fellows/admin.py
@@ -1,5 +1,7 @@
+"""Admin settings of fellow application"""
 from django.contrib import admin
-from .models import Fellow, Member, Leader
+
+from .models import Fellow, Leader, Member
 
 admin.site.register(Fellow)
 admin.site.register(Member)
