750ceef6657913f1760fc04ae0a2218a5b857192
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-04-15 01:07:24 +0900

    chore: videos model 수정

--- src/back-end/web/videos/models.py
+++ src/back-end/web/videos/models.py
@@ -1,16 +1,20 @@
+"""Videos App Model Definitions: Video, VideoDetail"""
+from django import forms
 from djongo import models
 
-class Videos(models.Model):
+
+class Video(models.Model):
+    """Video information that ott providers support"""
+
     CATEGORY_CHOICE = (
         ("TV", "TVSeries"),
         ("MV", "Movie"),
     )
 
-
-    id= models.BigAutoField(
+    id = models.BigAutoField(
         primary_key=True,
     )
-    tmdb_id = models.IntegerField(
+    tmdb_id = models.BigIntegerField(
         null=True,
         db_column="tmdbId",
     )
@@ -28,8 +32,8 @@ class Videos(models.Model):
         db_column="filmRating",
     )
     category = models.CharField(
-        max_length = 2,
-        null = True,
+        max_length=2,
+        null=True,
         choices=CATEGORY_CHOICE,
     )
     poster_key = models.ImageField(
@@ -38,43 +42,99 @@ class Videos(models.Model):
     title_english = models.CharField(
         max_length=200,
         null=False,
-        db_column="baseDate",
+        db_column="titleEnglish",
     )
 
     class Meta:
+        """DB table naming"""
+
         db_table = "videos"
 
     def __str__(self):
         return f"{self.name}"
 
 
+class Rating(models.Model):
+    """Abstract class to use ratings arrayfield"""
 
-class VideoDetails(models.Model):
-    id= models.BigAutoField(
+    name = models.CharField(
+        max_length=50,
+    )
+    value = models.FloatField(
+        max_length=10,
+    )
+
+    class Meta:
+        """type setting"""
+
+        abstract = True
+
+
+class RatingForm(forms.ModelForm):
+    """Form class to use ratings arrayfield"""
+
+    class Meta:
+        """class meta form setting"""
+
+        model = Rating
+        fields = ("name", "value")
+
+
+class ProductionCountry(models.Model):
+    """Abstract class to use production_countires arrayfield"""
+
+    name = models.CharField(max_length=2)
+
+    class Meta:
+        """type setting"""
+
+        abstract = True
+
+
+class Gerne(models.Model):
+    """Abstract class to use gernes arrayfield"""
+
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        """type setting"""
+
+        abstract = True
+
+
+class VideoDetail(models.Model):
+    """videoDetails models"""
+
+    id = models.BigAutoField(
         primary_key=True,
     )
-    video_id= models.ForeignKey(
-        Videos,
+    video_id = models.ForeignKey(
+        Video,
         on_delete=models.CASCADE,
         db_column="videoId",
     )
     runtime = models.IntegerField(
         null=False,
     )
-    rating = models.ArrayField(
-        null=False,
+    ratings = models.ArrayField(
+        model_container=Rating,
+        model_form_class=RatingForm,
+        null=False,  # model_container부분을 빼먹었다고 나옴 수정하겠슴.
     )
-    production_country= models.ArrayField(
-        max_length=2,
+    production_countries = models.ArrayField(
+        model_container=ProductionCountry,
         null=False,
-        db_column="productionCountry",
+        db_column="productionCountries",
     )
-    gernes= models.ArrayField(
+    gernes = models.ArrayField(
+        model_container=Gerne,
         max_length=50,
         null=True,
     )
 
     class Meta:
+        """DB table naming"""
+
         db_table = "video_details"
 
     def __str__(self):
