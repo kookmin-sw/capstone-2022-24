f12f6587906515b981a2ed81ce20c8af2ac55834
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-04-16 22:35:36 +0900

    chore: videos url.py 수정

--- src/back-end/web/videos/urls.py
+++ src/back-end/web/videos/urls.py
@@ -13,5 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.shortcuts import render
 
 urlpatterns = []
