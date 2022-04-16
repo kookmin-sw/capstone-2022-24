a66ab9cf1a306faef673d2b7c303f4affc4a610f
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-04-16 22:06:42 +0900

    Merge branch 'develop' of https://github.com/kookmin-sw/capstone-2022-24 into feature/038-be-videoModeling

--- src/front-end/src/mocks/handlers/oauth.js
+++ src/front-end/src/mocks/handlers/oauth.js
@@ -3,12 +3,21 @@ import { rest } from 'msw';
 export default [
 	// 네이버 로그인 mock data : status 301 (회원가입)
 	rest.post('/login/oauth/naver', (req, res, ctx) => {
-		return res(
-			ctx.status(301),
-		);
+		return res(ctx.status(301));
 	}),
 	// 구글 로그인 mock data : status 200 (로그인 성공)
 	rest.post('/login/oauth/google', (req, res, ctx) => {
+		return res(
+			ctx.status(200),
+			ctx.set({
+				Location: 'https://localhost:8080/join',
+			}),
+			ctx.json({
+				accessToken: 'ongaj.access.token',
+			}),
+		);
+	}),
+	rest.post('/users', (req, res, ctx) => {
 		return res(
 			ctx.status(200),
 			ctx.set({
