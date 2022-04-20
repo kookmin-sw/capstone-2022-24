4b67e795f488866786e2cbd3c5034f8157ac7e29
Dahee Goo <dadaahee@gmail.com>
2022-04-21 04:41:34 +0900

    feat: watching_marks 모델 및 시리얼라이저 작성

--- src/back-end/web/watching_marks/models.py
+++ src/back-end/web/watching_marks/models.py
@@ -1,5 +1,34 @@
 """Definitions of watching mark models: WatchingMark"""
+from django.conf import settings
+from django.utils import timezone
+from djongo import models
+from videos.models import Video
 
 
-class WatchingMark:
+class WatchingMark(models.Model):
     """Definition of whether to watch check model"""
+
+    id = models.BigAutoField(
+        primary_key=True,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=False,
+        on_delete=models.CASCADE,
+        db_column="userId",
+    )
+    video = models.ForeignKey(
+        Video,
+        null=False,
+        on_delete=models.CASCADE,
+        db_column="videoId",
+    )
+    date_time = models.DateTimeField(default=timezone.now, db_column="dateTime")
+
+    class Meta:
+        """Metadata for WatchingMark model"""
+
+        db_table = "watching_marks"
+
+    def __str__(self):
+        return f"{self.user}님이 {self.video} 작품을 관람하였습니다."
