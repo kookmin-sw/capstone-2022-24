50688d2ef89e5db22e4056a9cf916336d02cf04d
Dahee Goo <dadaahee@gmail.com>
2022-04-21 05:10:11 +0900

    chore: video 모델 뼈대만 남기고 삭제

--- src/back-end/web/videos/models.py
+++ src/back-end/web/videos/models.py
@@ -1,1 +1,10 @@
 """Videos App Model Definitions: Video, VideoDetail"""
+from djongo import models
+
+
+class Video(models.Model):
+    """Video information that ott providers support"""
+
+    id = models.BigAutoField(
+        primary_key=True,
+    )
