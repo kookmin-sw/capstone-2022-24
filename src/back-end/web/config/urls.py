1a6f8a0597cca3ad09643e5bcc26b78804a42874
Dahee Goo <dadaahee@gmail.com>
2022-04-22 03:46:01 +0900

    chore: urls.py에 API 문서 경로 추가

--- src/back-end/web/config/urls.py
+++ src/back-end/web/config/urls.py
@@ -1,1 +1,18 @@
 """URL configuration files"""
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularJSONAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("videos/", include("videos.urls")),
+    path("groups/", include("groups.urls")),
+    path("users/", include("users.urls")),
+    path("docs/schema/", SpectacularJSONAPIView.as_view(), name="docs"),  # api documentation file
+    path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="docs"), name="swagger"),  # api docs by swagger
+    path("docs/redoc/", SpectacularRedocView.as_view(url_name="docs"), name="redoc"),  # api docs by redoc
+]
