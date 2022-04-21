eaad8cb4203437129040b236b03c34ec1d49e66f
Dahee Goo <dadaahee@gmail.com>
2022-04-21 20:01:36 +0900

    chore: revert "chore: RecentView 모델 추가를 위한 Video 앱 임시 설정"
    This reverts commit 092d000d32bd5580fc3d31ee0c5509aff7ad889e.

--- src/back-end/web/videos/models.py
+++ src/back-end/web/videos/models.py
@@ -1,10 +1,3 @@
-"""Videos App Model Definitions: Video, VideoDetail"""
 from djongo import models
 
-
-class Video(models.Model):
-    """Video information that ott providers support"""
-
-    id = models.BigAutoField(
-        primary_key=True,
-    )
+# Create your models here.
