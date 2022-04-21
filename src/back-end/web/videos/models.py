88fc9a9a462f5e62c9aa572fe18a8c4e23c2916a
Dahee Goo <dadaahee@gmail.com>
2022-04-21 22:55:56 +0900

    chore: ci 테스트를 위해 Video 모델만 추가

--- src/back-end/web/videos/models.py
+++ src/back-end/web/videos/models.py
@@ -1,3 +1,10 @@
+"""Videos App Model Definitions: Video, VideoDetail"""
 from djongo import models
 
-# Create your models here.
+
+class Video(models.Model):
+    """Video information that ott providers support"""
+
+    id = models.BigAutoField(
+        primary_key=True,
+    )
