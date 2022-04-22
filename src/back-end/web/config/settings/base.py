9cca4b19d3c769ee9e3526cbdf92f0cf30f40079
Dahee Goo <dadaahee@gmail.com>
2022-04-22 19:00:23 +0900

    chore: pre-commit 훅에 따라 일부 수정

--- src/back-end/web/config/settings/base.py
+++ src/back-end/web/config/settings/base.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-# pylint: disable=R0801,W0401,W0614
 import os.path
 from pathlib import Path
 
