8ea480dc47d09fb86a938ba6dc474bf661beffbe
Dahee Goo <dadaahee@gmail.com>
2022-04-22 19:20:50 +0900

    feat: mariaDB로 전환, DB 타임존 변경

--- src/back-end/web/config/settings/local.py
+++ src/back-end/web/config/settings/local.py
@@ -7,42 +7,32 @@ from .base import *
 env = environ.Env(DEBUG=(bool, False))
 
 # read environment file
-environ.Env.read_env(env_file=os.path.join(ENV_DIR, '.env.local'))
+environ.Env.read_env(env_file=os.path.join(ENV_DIR, ".env.local"))
 
 DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ['[::1]', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["[::1]", "127.0.0.1", "localhost"]
 
 # Database
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': env('DB_NAME'),
-        'ENFORCE_SCHEMA': env('DB_ENFORCE_SCHEMA'),
-        'CLIENT': {
-            'host': env('DB_HOST_NAME'),
-            'port': int(env('DB_PORT')),
-            'username': env('DB_USER'),
-            'password': env('DB_PASSWORD'),
-            'authSource': 'admin',
-            'authMechanism': 'SCRAM-SHA-1',
-        },
-        'LOGGING': {
-            'version': 1,
-            'loggers': {
-                'djongo': {
-                    'level': 'DEBUG',
-                    'propagate': False,
-                }
-            },
-        },
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST_NAME"),
+        "PORT": int(env("DB_PORT")),
+        "TZ": env("TZ"),
+        "OPTIONS": {"init_command": 'SET sql_mode="STRICT_TRANS_TABLES"'},
     }
 }
 
 # static / media location in WAS(not s3 on local environment)
 STATIC_LOCATION = env("STATIC_LOCATION")
 MEDIA_LOCATION = env("MEDIA_LOCATION")
+
+# TZ
+TIME_ZONE = env("TZ")
