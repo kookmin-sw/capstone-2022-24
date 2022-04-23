0b20d8c948ac0df3171bf18809ade5b0e4540519
Dahee Goo <dadaahee@gmail.com>
2022-04-24 07:08:35 +0900

    refactor: djongo -> django.db 모델 import 변경

--- src/back-end/web/accounts/models.py
+++ src/back-end/web/accounts/models.py
@@ -1,21 +1,20 @@
+"""Definition of user's financial details in accounts application: Account, Bank"""
 from django.conf import settings
-from djongo import models
+from django.db import models
 
 
 class Bank(models.Model):
+    """Model Definition of bank in Korea"""
+
     id = models.BigAutoField(
         primary_key=True,
     )
-    code = models.CharField(
-        max_length=10,
-        null=False
-    )
-    name = models.CharField(
-        max_length=10,
-        null=False
-    )
+    code = models.CharField(max_length=10, null=False)
+    name = models.CharField(max_length=10, null=False)
 
     class Meta:
+        """Metadata for Bank model"""
+
         db_table = "banks"
 
     def __str__(self):
@@ -23,6 +22,8 @@ class Bank(models.Model):
 
 
 class Account(models.Model):
+    """Model Definition of account held by user"""
+
     id = models.BigAutoField(
         primary_key=True,
     )
@@ -32,18 +33,12 @@ class Account(models.Model):
         null=False,
         db_column="bankId",
     )
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null = False,
-        on_delete=models.CASCADE,
-        db_column="userId"
-    )
-    name = models.CharField(
-        max_length=10,
-        null=False
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, on_delete=models.CASCADE, db_column="userId")
+    name = models.CharField(max_length=10, null=False)
 
     class Meta:
+        """Metadata for Account model"""
+
         db_table = "accounts"
 
     def __str__(self):
