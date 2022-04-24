52c53383f8f6624c3b9bb188bafa4505cb8b2741
Dahee Goo <dadaahee@gmail.com>
2022-04-24 14:43:13 +0900

    refactor: djongo -> django.db 모델 import 변경, docstring 추가

--- src/back-end/web/group_accounts/models.py
+++ src/back-end/web/group_accounts/models.py
@@ -1,33 +1,19 @@
+"""Model definition of group_accounts application: GroupAccount"""
 from django.db import models
-from django.utils import timezone
 
 
 class GroupAccount(models.Model):
-    id = models.BigAutoField(
-        primary_key=True
-    )
-    identifier = models.CharField(
-        max_length=50,
-        null=True,
-        blank=True
-    )
-    password = models.CharField(
-        max_length=30,
-        null=True,
-        blank=True
-    )
-    creation_date_time = models.DateTimeField(
-        null=True,
-        blank=True,
-        db_column="creationDateTime"
-    )
-    last_modification_date_time = models.DateTimeField(
-        null=True,
-        blank=True,
-        db_column="lastModificationDateTime"
-    )
+    """Model definition of account used to share with members in a group"""
+
+    id = models.BigAutoField(primary_key=True)
+    identifier = models.CharField(max_length=50, null=True, blank=True)
+    password = models.CharField(max_length=30, null=True, blank=True)
+    creation_date_time = models.DateTimeField(null=True, blank=True, db_column="creationDateTime")
+    last_modification_date_time = models.DateTimeField(null=True, blank=True, db_column="lastModificationDateTime")
 
     class Meta:
+        """Metadata of GroupAccount model"""
+
         db_table = "group_accounts"
 
     def __str__(self):
