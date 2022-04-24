eb3c85f75cdaf981ad3417822f67237e11694dee
Dahee Goo <dadaahee@gmail.com>
2022-04-24 14:57:32 +0900

    refactor: djongo -> django.db 모델 import 변경, docstring 추가, singular table명으로 변경

--- src/back-end/web/users/models.py
+++ src/back-end/web/users/models.py
@@ -1,64 +1,63 @@
+"""Model definition of users application: SocialType, UserManager, User"""
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
 from django.utils import timezone
-from djongo import models
 
 
 class SocialType(models.Model):
-    SOCIAL_PLATFORM_CHOICES = (
-        ('N', 'Naver'),
-        ('G', 'Google'),
-    )
-    id = models.BigAutoField(
-        primary_key=True
-    )
-    name = models.CharField(
-        max_length=1,
-        choices=SOCIAL_PLATFORM_CHOICES
-    )
-    logo_key = models.CharField(
-        max_length=100,
-        db_column="logoKey"
-    )
+    """Model of sns platform that user enters"""
 
-    class Meta:
-        db_table = "social_types"
+    SOCIAL_PLATFORM_CHOICES = (("N", "Naver"), ("G", "Google"), ("O", "Others"))
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=1, choices=SOCIAL_PLATFORM_CHOICES)
+    logo_key = models.CharField(max_length=100, db_column="logoKey")
 
     def __str__(self):
         return f"{self.name}"
 
 
 class UserManager(BaseUserManager):
+    """Model of managing user object crud"""
+
+    # pylint: disable=R0913
     def create_user(self, nickname, email, cell_phone_number, social_type, birthday, password=None):
+        """Manage to create normal user"""
 
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
 
         if not cell_phone_number:
-            raise ValueError('Users must have an cell phone number')
+            raise ValueError("Users must have an cell phone number")
 
         if not birthday:
-            raise ValueError('Users must have birthday')
+            raise ValueError("Users must have birthday")
 
         user = self.model(
             nickname=nickname,
             email=self.normalize_email(email),
             cell_phone_number=cell_phone_number,
             social_type=social_type,
-            birthday=birthday
+            birthday=birthday,
         )
 
         user.set_unusable_password()
         user.save(using=self._db)
         return user
 
+    # pylint: disable=R0913
     def create_superuser(self, nickname, email, cell_phone_number, birthday, password, social_type=None):
+        """Manage to create superuser"""
+
+        if not social_type:
+            social_type = SocialType(1, "O", "Not found")
+            social_type.save(using=self._db)
         user = self.create_user(
             nickname=nickname,
             email=email,
             cell_phone_number=cell_phone_number,
             birthday=birthday,
             password=password,
-            social_type=social_type
+            social_type=social_type,
         )
         user.is_admin = True
         user.set_password(password)
@@ -67,58 +66,23 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, models.Model):
-    id = models.BigAutoField(
-        primary_key=True
-    )
-    nickname = models.CharField(
-        max_length=8,
-        unique=True
-    )
+    """Model of user that use ongot service"""
+
+    id = models.BigAutoField(primary_key=True)
+    nickname = models.CharField(max_length=8, unique=True)
     email = models.EmailField(
         max_length=50,
     )
-    cell_phone_number = models.CharField(
-        max_length=14,
-        db_column="cellPhoneNumber"
-    )
-    social_type = models.ForeignKey(
-        SocialType,
-        default=None,
-        db_column="socialTypeId",
-        on_delete=models.SET_DEFAULT
-    )
-    profile_image_url = models.ImageField(
-        blank=True,
-        null=True,
-        db_column="profileImageUrl"
-    )
+    cell_phone_number = models.CharField(max_length=14, db_column="cellPhoneNumber")
+    social_type = models.ForeignKey(SocialType, default=None, db_column="socialTypeId", on_delete=models.SET_DEFAULT)
+    profile_image_url = models.ImageField(blank=True, null=True, db_column="profileImageUrl")
     birthday = models.DateField()
-    is_active = models.BooleanField(
-        default=True,
-        db_column="isActive"
-    )
-    is_admin = models.BooleanField(
-        default=False,
-        db_column="isAdmin"
-    )
-    total_mileages = models.PositiveIntegerField(
-        default=0,
-        db_column="totalMileages"
-    )
-    withdrawal_date_time = models.DateTimeField(
-        null=True,
-        blank=True,
-        db_column="withdrawalDateTime"
-    )
-    registration_date_time = models.DateTimeField(
-        default=timezone.now,
-        db_column="registrationDateTime"
-    )
-    last_login_date_time = models.DateTimeField(
-        blank=True,
-        null=True,
-        db_column="lastLoginDateTime"
-    )
+    is_active = models.BooleanField(default=True, db_column="isActive")
+    is_admin = models.BooleanField(default=False, db_column="isAdmin")
+    total_mileages = models.PositiveIntegerField(default=0, db_column="totalMileages")
+    withdrawal_date_time = models.DateTimeField(null=True, blank=True, db_column="withdrawalDateTime")
+    registration_date_time = models.DateTimeField(default=timezone.now, db_column="registrationDateTime")
+    last_login_date_time = models.DateTimeField(blank=True, null=True, db_column="lastLoginDateTime")
 
     USERNAME_FIELD = "nickname"
     EMAIL_FIELD = "email"
@@ -127,18 +91,22 @@ class User(AbstractBaseUser, models.Model):
     REQUIRED_FIELDS = ["email", "cell_phone_number", "birthday"]
 
     class Meta:
-        db_table = "users"
-        ordering = ('-registration_date_time',)
+        """Metadata of User model"""
+
+        ordering = ("-registration_date_time",)
 
     def __str__(self):
         return f"{self.nickname}"
 
     def has_perm(self, perm, obj=None):
+        """Always has perms"""
         return True
 
     def has_module_perms(self, app_label):
+        """Always has module perms"""
         return True
 
     @property
     def is_staff(self):
+        """Isn't normal but admin?"""
         return self.is_admin
