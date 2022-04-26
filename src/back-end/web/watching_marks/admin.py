9456674efdb92ab8b00e7635e608476c8f51bb71
Dahee Goo <dadaahee@gmail.com>
2022-04-26 18:13:38 +0900

    style: watching_marks 어플리케이션 코드 스타일 통일

--- src/back-end/web/watching_marks/admin.py
+++ src/back-end/web/watching_marks/admin.py
@@ -1,6 +1,5 @@
 """Admin settings of watching_marks application"""
 from django.contrib import admin
-
-from .models import WatchingMark
+from watching_marks.models import WatchingMark
 
 admin.site.register(WatchingMark)
