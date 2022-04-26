7da4d120570b0a16be46655e6f81d6202c656e71
Dahee Goo <dadaahee@gmail.com>
2022-04-26 17:56:51 +0900

    style: providers 어플리케이션 코드 스타일 통일

--- src/back-end/web/providers/serializers.py
+++ src/back-end/web/providers/serializers.py
@@ -1,31 +1,45 @@
-from djongo import models
-
-from .models import Provider, Charge, SubscriptionType
+"""Serializers of providers application for json parsing"""
+from providers.models import Charge, Provider, SubscriptionType
 from rest_framework import serializers
 
 
 class ProviderSerializer(serializers.ModelSerializer):
+    """provider model serializer in providers application"""
+
     class Meta:
+        """Meatadata for ProviderSerializer"""
+
         model = Provider
-        fields = '__all__'
+        fields = "__all__"
 
 
 class SubscriptionTypeSerializer(serializers.ModelSerializer):
+    """SubscriptionType model serializer for json parsing"""
+
     class Meta:
+        """Metadata of SubscriptionTypeSerializer"""
+
         model = SubscriptionType
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ChargeSerializer(serializers.ModelSerializer):
+    """Charge model serializer in providers application"""
+
     provider = serializers.SerializerMethodField()
     subscriptionType = serializers.SerializerMethodField()
 
     class Meta:
+        """Meatadata for ChargeSerializer"""
+
         model = Charge
-        fields = '__all__'
+        fields = "__all__"
 
     def get_provider(self, obj):
+        """Get proivder data using ProviderSerializer"""
         return ProviderSerializer(obj.provider).data
 
     def get_subscription_type(self, obj):
+        """Get subscription type data using SubscriptionTypeSerializer"""
+
         return SubscriptionTypeSerializer(obj.subscriptionType).data
