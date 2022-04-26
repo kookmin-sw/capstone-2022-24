ffb4ce90ca007cd81dd551271fd241b64f738026
Dahee Goo <dadaahee@gmail.com>
2022-04-26 17:44:53 +0900

    refactor: 자동 생성되는 id 필드 제거

--- src/back-end/web/group_accounts/models.py
+++ src/back-end/web/group_accounts/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class GroupAccount(models.Model):
     """Model definition of account used to share with members in a group"""
 
-    id = models.BigAutoField(primary_key=True)
     identifier = models.CharField(max_length=50, null=True, blank=True)
     password = models.CharField(max_length=30, null=True, blank=True)
     creation_date_time = models.DateTimeField(null=True, blank=True)
