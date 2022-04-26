06ffaf39a5ef55e95f6a43995efb45c8a854c8ab
Dahee Goo <dadaahee@gmail.com>
2022-04-26 18:12:51 +0900

    refactor: Video 모델에서 id 필드 제거 (장고 내부에서 자동 생성됨)

--- src/back-end/web/videos/models.py
+++ src/back-end/web/videos/models.py
@@ -5,10 +5,6 @@ from django.db import models
 class Video(models.Model):
     """Video information that ott providers support"""
 
-    id = models.BigAutoField(
-        primary_key=True,
-    )
-
     class Meta:
         """Metadata for video model"""
 
