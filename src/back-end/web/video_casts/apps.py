8052809fc26758d5a84c4a19b062b8291d725bc9
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-04-30 20:01:54 +0900

    Update: video_casts APP 추가

---
+++ src/back-end/web/video_casts/apps.py
@@ -0,0 +1,9 @@
+"""Configuration of video casts"""
+from django.apps import AppConfig
+
+
+class VideoCastsConfig(AppConfig):
+    """Configuration details of video casts"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "video_casts"
