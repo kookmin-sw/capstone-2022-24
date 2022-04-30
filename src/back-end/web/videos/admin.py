58c2e045a1f115118405dd3414e252b8d8c61a0f
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-01 02:44:33 +0900

    chore: admin 파일 일괄 수정

--- src/back-end/web/videos/admin.py
+++ src/back-end/web/videos/admin.py
@@ -1,7 +1,9 @@
 """Admin : Video, VideoDetail"""
 from django.contrib import admin
-
-from .models import Video, VideoDetail
+from videos.models import Gerne, ProductionCountry, Rating, Video, VideoDetail
 
 admin.site.register(Video)
 admin.site.register(VideoDetail)
+admin.site.register(Rating)
+admin.site.register(Gerne)
+admin.site.register(ProductionCountry)
