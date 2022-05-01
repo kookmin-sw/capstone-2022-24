0f7cd3672f1a2f45fd1bbe3565c1df1b7502d059
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-01 15:00:21 +0900

    chore: video App의 serializer 수정

--- src/back-end/web/videos/serializer.py
+++ src/back-end/web/videos/serializer.py
@@ -1,30 +1,80 @@
 """Videos App Model Serializer: Video, VideoDetail"""
 from rest_framework import serializers
+from videos.models import Gerne, ProductionCountry, Rating, Video, VideoDetail
 
-from .models import Video, VideoDetail
 
-
-class VideosSerialaizer(serializers.ModelSerializer):
-    """Video information Serializing to get data"""
+class VideoSerialaizer(serializers.ModelSerializer):
+    """Video model Serializer in videos application"""
 
     class Meta:
-        """Video Serializer field setting"""
+        """Metadata for videos Serializer"""
 
         model = Video
-        fields = ["tmdb_id", "title", "release_date", "film_rating", "category", "poster_key", "title_english"]
+        fields = "__all__"
 
 
-class VideosDetailsSerialaizer(serializers.ModelSerializer):
-    """Video detail information Serializing to get data"""
+class VideoDetailSerialaizer(serializers.ModelSerializer):
+    """VideoDetail model Serializer in videos application"""
 
     video = serializers.SerializerMethodField()
 
     def get_video(self, obj):
-        """Method : get to Video data"""
-        return VideosSerialaizer(obj.video).data
+        """Method : get to Video data Using VideosDetailsSerialaizer"""
+        return VideoSerialaizer(obj.video).data
 
     class Meta:
-        """Video Serializer field setting"""
+        """Metadata for videos details Serializer"""
 
         model = VideoDetail
-        fields = ["runtime", "ratings", "production_countries", "gernes"]
+        fields = "__all__"
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    """Rating model Serializer in videos application"""
+
+    video_details = serializers.SerializerMethodField()
+
+    def get_video_detail(self, obj):
+        """Method : get to Video detail data Using RatingSerializer"""
+
+        return VideoDetailSerialaizer(obj.video_details).data
+
+    class Meta:
+        """Metadata for video rating Serializer"""
+
+        model = Rating
+        fields = "__all__"
+
+
+class ProductionCountrySerializer(serializers.ModelSerializer):
+    """ProductionCountry model Serializer in videos application"""
+
+    video_details = serializers.SerializerMethodField()
+
+    def get_video_detail(self, obj):
+        """Method : get to Video detail data Using ProductionCountrySerializer"""
+
+        return VideoDetailSerialaizer(obj.video_details).data
+
+    class Meta:
+        """Metadata for video Production Country Serializer"""
+
+        model = ProductionCountry
+        fields = "__all__"
+
+
+class GernesSerializer(serializers.ModelSerializer):
+    """VideoDetail model Serializer in videos application"""
+
+    video_details = serializers.SerializerMethodField()
+
+    def get_video_detail(self, obj):
+        """Method : get to Video detail data Using GernesSerializer"""
+
+        return VideoDetailSerialaizer(obj.video_details).data
+
+    class Meta:
+        """Metadata for video Gernes Serializer"""
+
+        model = Gerne
+        fields = "__all__"
