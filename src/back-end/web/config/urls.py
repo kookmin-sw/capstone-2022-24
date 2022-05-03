ced5586b2787dc743bc414c7c18019295385bbea
Dahee Goo <dadaahee@gmail.com>
2022-05-03 20:32:28 +0900

    chore: urls.py 주석 추가 및 구조화

--- src/back-end/web/config/urls.py
+++ src/back-end/web/config/urls.py
@@ -12,10 +12,8 @@ urlpatterns = [
     path("videos/", include("videos.urls")),
     path("groups/", include("groups.urls")),
     path("users/", include("users.urls")),
+    # api docs
     path("docs/schema/", SpectacularJSONAPIView.as_view(), name="docs"),  # api documentation file
     path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="docs"), name="swagger"),  # api docs by swagger
     path("docs/redoc/", SpectacularRedocView.as_view(url_name="docs"), name="redoc"),  # api docs by redoc
-    # auth
-    path("users/", include("dj_rest_auth.urls")),
-    path("users/", include("allauth.urls")),
 ]
