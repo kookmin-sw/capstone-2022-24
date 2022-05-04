970153d584d30bd1148e328ecfe22767b08d6db8
Dahee Goo <dadaahee@gmail.com>
2022-05-05 02:50:49 +0900

    feat: 네이버/구글 로그인 구현

--- src/back-end/web/users/urls.py
+++ src/back-end/web/users/urls.py
@@ -2,21 +2,24 @@
 from django.urls import include, path
 
 # router = DefaultRouter()
-# router.register(r'', views.UserViewSet)
+# router.register(r"", views.UserViewSet)
+from users.views import NaverLogin, GoogleLogin
 
 urlpatterns = [
     # path("", include(router.urls)),
     path("", include("dj_rest_auth.urls")),
     path("", include("dj_rest_auth.registration.urls")),
     path("", include("allauth.urls")),
-    # path('login/oauth/', views.google_login, name='google_login'),
-    # path('google/callback/', views.google_callback, name='google_callback'),
-    # path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
+    path("login/oauth/naver/", NaverLogin.as_view(), name="naver_login"),
+    path("login/oauth/google/", GoogleLogin.as_view(), name="google_login")
+    # path("login/oauth/", views.google_login, name="google_login"),
+    # path("google/callback/", views.google_callback, name="google_callback"),
+    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_todjango"),
     # path("login/oauth/google/", views.GoogleLogin.as_view(), name="google_login"),
     # path("login/oauth/naver/", views.NaverLogin.as_view(), name="naver_login"),
-    # path('google/login/{', views.google_login, name='google_login'),
-    # path('google/callback/', views.google_callback,      name='google_callback'),
-    # path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
+    # path("google/login/{", views.google_login, name="google_login"),
+    # path("google/callback/", views.google_callback,      name="google_callback"),
+    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_todjango"),
 ]
 
 # /login/oauth/{social_type}
--- src/back-end/web/users/views.py
+++ src/back-end/web/users/views.py
@@ -1,1 +1,16 @@
 """APIs of users application"""
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.naver.views import NaverOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+
+
+class NaverLogin(SocialLoginView):
+    adapter_class = NaverOAuth2Adapter
+    client_class = OAuth2Client
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+
