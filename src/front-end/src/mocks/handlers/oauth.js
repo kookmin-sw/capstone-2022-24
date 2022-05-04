3115916e2eacf3966e8f95c2e110a450c4bf2060
Dahee Goo <dadaahee@gmail.com>
2022-05-05 00:03:29 +0900

    fix: 소셜 로그인 경로 수정

--- src/front-end/src/mocks/handlers/oauth.js
+++ src/front-end/src/mocks/handlers/oauth.js
@@ -2,11 +2,11 @@ import { rest } from 'msw';
 
 export default [
 	// 네이버 로그인 mock data : status 301 (회원가입)
-	rest.post('/login/oauth/naver', (req, res, ctx) => {
+	rest.post('/users/naver/login', (req, res, ctx) => {
 		return res(ctx.status(301));
 	}),
 	// 구글 로그인 mock data : status 200 (로그인 성공)
-	rest.post('/login/oauth/google', (req, res, ctx) => {
+	rest.post('/users/google/login', (req, res, ctx) => {
 		return res(
 			ctx.status(200),
 			ctx.set({
