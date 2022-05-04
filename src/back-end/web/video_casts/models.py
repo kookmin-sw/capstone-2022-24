76b476d06384dca1cd16ba1e4799abd20d32351d
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-04 20:35:58 +0900

    chore: model에 choice 추가, 수정

--- src/back-end/web/video_casts/models.py
+++ src/back-end/web/video_casts/models.py
@@ -6,6 +6,21 @@ from videos.models import Video
 class VideoCast(models.Model):
     """Definition of information about casts who appeared in the video"""
 
+    DEPARTMENT_CHOICE = (
+        ("Cast"),
+        ("Crew"),
+        ("Sound"),
+        ("Lighting"),
+        ("Production"),
+        ("Art"),
+        ("Editing"),
+        ("Costume & Make-Up"),
+        ("Camera"),
+        ("Directing"),
+        ("Visual Effects"),
+        ("Actors"),
+        ("Writing"),
+    )
     video = models.ForeignKey(
         Video,
         on_delete=models.CASCADE,
@@ -14,15 +29,12 @@ class VideoCast(models.Model):
         max_length=30,
     )
     department = models.CharField(
-        max_length=30,
+        max_length=20,
         null=True,
+        choices=DEPARTMENT_CHOICE,
     )
     role = models.CharField(
         max_length=50,
-        null=True,
-    )
-    character = models.CharField(
-        max_length=30,
         null=True,
     )
 
@@ -32,4 +44,4 @@ class VideoCast(models.Model):
         db_table = "video_casts"
 
     def __str__(self):
-        return f"작품 {self.video.title}의 출연진/제작진 {self.name}"
+        return f"출연진/제작진 {self.name}"
