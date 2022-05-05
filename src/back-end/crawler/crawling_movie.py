343aaad2c595bae9db29312f2b5cd4d42ed2e78a
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-05 17:53:28 +0900

    chore: 일부 이름 수정, limit 설정 수정

--- src/back-end/crawler/crawling_movie.py
+++ src/back-end/crawler/crawling_movie.py
@@ -15,7 +15,7 @@ def dict_movie_update(data_path):
         total_pages = json_ob["total_pages"]
 
         for i in range(1, total_pages + 1):
-            if i <= 500:
+            if i <= MAX_PAGE_LIMIT:
                 page_num = i
                 url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language={language}&sort_by=popularity.desc&certification_country=KR&page={page_num}&with_watch_providers={provider}&watch_region={watch_region}"
                 json_obj = get_request_to_object(url)
@@ -27,7 +27,7 @@ def dict_movie_update(data_path):
                     title = json_obj["results"][j]["title"]
                     Movie_dict[id] = {"tmdb_id": id, "title": title, "Category": "Movie"}
             else:
-                page_num = i - 500
+                page_num = i - MAX_PAGE_LIMIT
                 url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language={language}&sort_by=popularity.desc&certification_country=KR&page={page_num}&with_watch_providers={provider}&watch_region={watch_region}"
                 json_obj = get_request_to_object(url)
                 count = len(json_obj["results"])
--- src/back-end/crawler/saving_DB.py
+++ src/back-end/crawler/saving_DB.py
@@ -17,7 +17,7 @@ from check_env import setting_env
 """==========Model for Saving data==========="""
 
 
-def saving_video_data(video_data):
+def save_video_data(video_data):
     """Method: Save the data in Video model"""
     for item in video_data:
         sql = "INSERT INTO videos (tmdb_id, title, release_date, film_rating,category,poster_key,title_english) VALUES (%s, %s, %s,%s,%s,%s,%s)"
@@ -34,7 +34,7 @@ def saving_video_data(video_data):
         conn.commit()
 
 
-def saving_detail_data(video_detail_data):
+def save_detail_data(video_detail_data):
     """Method: Save the data in VideoDetail model"""
     for item in video_detail_data:
         sql_id = "SELECT id FROM videos WHERE tmdb_id =%s AND category= %s"
@@ -51,7 +51,7 @@ def saving_detail_data(video_detail_data):
         conn.commit()
 
 
-def saving_genre_data(video_genre_data):
+def save_genre_data(video_genre_data):
     """Method: Save the data in Genre model"""
     for item in video_genre_data:
         sql_id = "SELECT id FROM videos WHERE tmdb_id =%s AND category= %s"
@@ -67,7 +67,7 @@ def saving_genre_data(video_genre_data):
             conn.commit()
 
 
-def saving_production_country_data(video_production_country_data):
+def save_production_country_data(video_production_country_data):
     """Method: Save the data in ProductionCountry model"""
     for item in video_production_country_data:
         sql_id = "SELECT id FROM videos WHERE tmdb_id =%s AND category= %s"
@@ -83,7 +83,7 @@ def saving_production_country_data(video_production_country_data):
             conn.commit()
 
 
-def saving_provider_data(video_provider_data):
+def save_provider_data(video_provider_data):
     """Method: Save the data in VideoProvider model"""
     for item in video_provider_data:
         sql_id = "SELECT id FROM videos WHERE tmdb_id =%s AND category= %s"
@@ -151,11 +151,11 @@ if __name__ == "__main__":
     movie_detail_data, movie_genre_data, movie_production_country_data = arrange_movie_detail(json_movie_dict)
     movie_provider_data = arrange_movie_provider(json_movie_dict)
 
-    saving_video_data(movie_data)
-    saving_detail_data(movie_detail_data)
-    saving_genre_data(movie_genre_data)
-    saving_production_country_data(movie_production_country_data)
-    saving_provider_data(movie_provider_data)
+    save_video_data(movie_data)
+    save_detail_data(movie_detail_data)
+    save_genre_data(movie_genre_data)
+    save_production_country_data(movie_production_country_data)
+    save_provider_data(movie_provider_data)
 
     """==========Tv data Saving==========="""
 
@@ -170,10 +170,10 @@ if __name__ == "__main__":
     tv_detail_data, tv_genre_data, tv_production_country_data = arrange_tv_detail(json_tv_dict)
     tv_provider_data = arrange_tv_provider(json_tv_dict)
 
-    saving_video_data(tv_data)
-    saving_detail_data(tv_detail_data)
-    saving_genre_data(tv_genre_data)
-    saving_production_country_data(tv_production_country_data)
-    saving_provider_data(tv_provider_data)
+    save_video_data(tv_data)
+    save_detail_data(tv_detail_data)
+    save_genre_data(tv_genre_data)
+    save_production_country_data(tv_production_country_data)
+    save_provider_data(tv_provider_data)
 
     conn.close()
