c1be3de0d49544a6a35aae54b2cb2c5ecced05b4
Dahee Goo <dadaahee@gmail.com>
2022-05-06 04:47:02 +0900

    chore: 사용자 인증 관련 urls 등록 및 정리

--- src/back-end/web/users/urls.py
+++ src/back-end/web/users/urls.py
@@ -1,29 +1,64 @@
 """Users URL Configuration"""
-from django.urls import include, path
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.views import LoginView, LogoutView
+from django.urls import path
 
-# router = DefaultRouter()
-# router.register(r"", views.UserViewSet)
-from users.views import NaverLogin, GoogleLogin
+from users.views import (
+    SignUpView,
+    NaverLoginView,
+    GoogleLoginView,
+    ValidateNicknameView,
+    ValidateProfileImageView,
+    ValidateWithdrawalView,
+    ProfileImageCreateView,
+    UserWithdrawalView,
+)
 
 urlpatterns = [
-    # path("", include(router.urls)),
-    path("", include("dj_rest_auth.urls")),
-    path("", include("dj_rest_auth.registration.urls")),
-    path("", include("allauth.urls")),
-    path("login/oauth/naver/", NaverLogin.as_view(), name="naver_login"),
-    path("login/oauth/google/", GoogleLogin.as_view(), name="google_login")
-    # path("login/oauth/", views.google_login, name="google_login"),
-    # path("google/callback/", views.google_callback, name="google_callback"),
-    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_todjango"),
-    # path("login/oauth/google/", views.GoogleLogin.as_view(), name="google_login"),
-    # path("login/oauth/naver/", views.NaverLogin.as_view(), name="naver_login"),
-    # path("google/login/{", views.google_login, name="google_login"),
-    # path("google/callback/", views.google_callback,      name="google_callback"),
-    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_todjango"),
+    # Supported apis
+    # - GET /users/user/
+    # - PUT /users/user/
+    # - PATCH /users/user/
+    # - POST /users/verify-email/
+    # - POST /users/login/
+    # - POST /users/logout/
+    # - POST /users/password/change/
+    # - POST /users/password/reset/
+    # - POST /users/password/reset/confirm/
+    # - POST /uesrs/token/refresh/
+    # - POST /users/token/verify/
+    # path("", include("dj_rest_auth.urls")),
+
+    # oauth2 login
+    path("login/oauth/naver/", NaverLoginView.as_view(), name="users_login_oauth_naver"),
+    path("login/oauth/google/", GoogleLoginView.as_view(), name="users_login_oauth_google"),
+
+    # Supported api
+    # - POST /users/
+    # - POST /users/resend-email/
+    # - POST /users/verify-email/
+    # path("", include("dj_rest_auth.registration.urls")),
+
+    # sign up
+    path("", SignUpView.as_view(), name="users_signup"),
+
+    # token refresh
+    path("token/", get_refresh_view().as_view(), name="token_refresh"),
+
+    # normal login / logout
+    path("login/", LoginView.as_view(), name="login"),  # 일반 로그인 (미사용)
+    path("logout/", LogoutView.as_view(), name="logout"),  # 로그아웃 (미사용)
+
+    # validate user fields
+    path("validate-nickname/", ValidateNicknameView.as_view(), name="users_validate_nickname"),
+    path("validate-profile-image/", ValidateProfileImageView.as_view(), name="users_alidate_profile_image"),
+    path("profile-image/", ProfileImageCreateView.as_view(), name="users_upload_profile_image"),
+
+    # withdrawal
+    path("<str:nickname>/validate-withdrawal/", ValidateWithdrawalView.as_view(), name="users_nickname_validate_withdrawal"),
+    path("", UserWithdrawalView.as_view(), name="users_withdrawal")
 ]
 
-# /login/oauth/{social_type}
-# /users
 # /token
 # /login
 # /logout
