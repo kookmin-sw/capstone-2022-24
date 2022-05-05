698a9bceb086ff21df43d31d6b7d6be97becb8fa
Dahee Goo <dadaahee@gmail.com>
2022-05-06 04:50:28 +0900

    chore: urls.py path 코드 스타일 변경 - 인자별 엔터 처리

--- src/back-end/web/users/urls.py
+++ src/back-end/web/users/urls.py
@@ -15,55 +15,70 @@ from users.views import (
 )
 
 urlpatterns = [
-    # Supported apis
-    # - GET /users/user/
-    # - PUT /users/user/
-    # - PATCH /users/user/
-    # - POST /users/verify-email/
-    # - POST /users/login/
-    # - POST /users/logout/
-    # - POST /users/password/change/
-    # - POST /users/password/reset/
-    # - POST /users/password/reset/confirm/
-    # - POST /uesrs/token/refresh/
-    # - POST /users/token/verify/
-    # path("", include("dj_rest_auth.urls")),
-
     # oauth2 login
-    path("login/oauth/naver/", NaverLoginView.as_view(), name="users_login_oauth_naver"),
-    path("login/oauth/google/", GoogleLoginView.as_view(), name="users_login_oauth_google"),
-
-    # Supported api
-    # - POST /users/
-    # - POST /users/resend-email/
-    # - POST /users/verify-email/
-    # path("", include("dj_rest_auth.registration.urls")),
+    path(
+        "login/oauth/naver/",
+        NaverLoginView.as_view(),
+        name="users_login_oauth_naver"
+    ),
+    path(
+        "login/oauth/google/",
+        GoogleLoginView.as_view(),
+        name="users_login_oauth_google"
+    ),
 
     # sign up
-    path("", SignUpView.as_view(), name="users_signup"),
+    path(
+        "",
+        SignUpView.as_view(),
+        name="users_signup"
+    ),
 
     # token refresh
-    path("token/", get_refresh_view().as_view(), name="token_refresh"),
+    path(
+        "token/",
+        get_refresh_view().as_view(),
+        name="token_refresh"
+    ),
 
-    # normal login / logout
-    path("login/", LoginView.as_view(), name="login"),  # 일반 로그인 (미사용)
-    path("logout/", LogoutView.as_view(), name="logout"),  # 로그아웃 (미사용)
+    # normal login / logout (maaaay be used)
+    path(
+        "login/",
+        LoginView.as_view(),
+        name="login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(),
+        name="logout"
+    ),
 
     # validate user fields
-    path("validate-nickname/", ValidateNicknameView.as_view(), name="users_validate_nickname"),
-    path("validate-profile-image/", ValidateProfileImageView.as_view(), name="users_alidate_profile_image"),
-    path("profile-image/", ProfileImageCreateView.as_view(), name="users_upload_profile_image"),
+    path(
+        "validate-nickname/",
+        ValidateNicknameView.as_view(),
+        name="users_validate_nickname"
+    ),
+    path(
+        "validate-profile-image/",
+        ValidateProfileImageView.as_view(),
+        name="users_alidate_profile_image"
+    ),
+    path(
+        "profile-image/",
+        ProfileImageCreateView.as_view(),
+        name="users_upload_profile_image"
+    ),
 
     # withdrawal
-    path("<str:nickname>/validate-withdrawal/", ValidateWithdrawalView.as_view(), name="users_nickname_validate_withdrawal"),
-    path("", UserWithdrawalView.as_view(), name="users_withdrawal")
+    path(
+        "<str:nickname>/validate-withdrawal/",
+        ValidateWithdrawalView.as_view(),
+        name="users_nickname_validate_withdrawal"
+    ),
+    path(
+        "",
+        UserWithdrawalView.as_view(),
+        name="users_withdrawal"
+    )
 ]
-
-# /token
-# /login
-# /logout
-# /users/validate-nickname
-# /users/validate-profile-images
-# /users/profile-images
-# /users/{nickname}/validate-withdrawal
-# /users
