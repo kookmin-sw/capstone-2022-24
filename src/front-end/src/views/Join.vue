ff03d8b582c085fd772e91e9f1e4034c2931f449
yesslee <lys0301174@kookmin.ac.kr>
2022-05-07 22:04:50 +0900

    design: 2단계 모임장/모임원 선택 버튼 테두리 적용방식 css 클래스 방식으로 변경

--- src/front-end/src/views/Join.vue
+++ src/front-end/src/views/Join.vue
@@ -54,7 +54,7 @@
 			<q-btn-toggle
 				v-model="role"
 				spread
-				class="role-toggle-button"
+				class="border-blue-200"
 				unelevated
 				toggle-color="blue-4"
 				text-color="blue-4"
@@ -170,8 +170,5 @@ export default {
 .ott-select {
 	border: 3px solid #449bfe; /*blue-200*/
 	border-radius: 4px;
-}
-.role-toggle-button {
-	border: 1px solid #449bfe; /*blue-200*/
 }
 </style>
