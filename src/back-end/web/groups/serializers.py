55e2f8b341a9ff8161333c872bcd9fd0a843162d
Dahee Goo <dadaahee@gmail.com>
2022-05-11 08:47:53 +0900

    feat: GroupSerializer - default, others로 구분하여 구조화

--- src/back-end/web/groups/serializers.py
+++ src/back-end/web/groups/serializers.py
@@ -1,10 +1,7 @@
 """Serializers of groups application for json parsing"""
 from group_accounts.serializers import GroupAccountSerializer
 from groups.models import Group
-from providers.serializers import (
-    MyPageGroupOthersProviderSerializer,
-    ProviderSerializer,
-)
+from providers.serializers import ProviderSerializer
 from rest_framework import serializers
 
 
@@ -30,18 +27,58 @@ class GroupSerializer(serializers.ModelSerializer):
         return GroupAccountSerializer(obj.group_account).data
 
 
-class MyPageGroupSerializer(serializers.ModelSerializer):
-    """Group serailizer in mypage format"""
+class GroupTimeStampSerializer(serializers.ModelSerializer):
+    """Date time stamps in group serializer"""
 
-    default = {}
-    others = {}
+    class Meta:
+        """Metadata for GroupTimeStampSerializer"""
+
+        model = Group
+        fields = [
+            "creation_date_time",
+            "start_watching_date_time",
+            "end_watching_date_time",
+            "end_reporting_date_time",
+        ]
 
 
 class MyPageGroupDefaultSerializer(serializers.ModelSerializer):
     """Default group serailizer in mypage format"""
 
-    default = {}
-    others = {}
+    provider = serializers.SerializerMethodField()
+    time_stamps = serializers.SerializerMethodField()
+    report = serializers.SerializerMethodField()
+    account = serializers.SerializerMethodField()
+    fellows = serializers.SerializerMethodField()
+
+    class Meta:
+        """Metadata of MyPageGroupDefaultSerializer"""
+
+        model = Group
+        fields = [
+            "status",
+        ]
+
+    def get_provider(self, group):
+        """Get provider that group subscribes"""
+        return ProviderSerializer(group.provider).data
+
+    def get_time_stamps(self, group):
+        """watching / reporting / creation date time details"""
+        return GroupTimeStampSerializer(group).data
+
+    def get_report(self, group):
+        """Calculate counts fellows reported"""
+        # TODO
+        return
+
+    def get_account(self, group):
+        """Account details(id & pw & timestamps) that group leader registers"""
+        return GroupAccountSerializer(group.group_account).data
+
+    def get_fellows(self, group):
+        """Get fellows related in the group"""
+        # TODO
 
 
 class MyPageGroupOthersSerializer(serializers.ModelSerializer):
@@ -55,6 +92,19 @@ class MyPageGroupOthersSerializer(serializers.ModelSerializer):
         model = Group
         read_only_fields = "__all__"
 
-    def get_provider(self, obj):
+    def get_provider(self, group):
         """Get provider of one of others' group"""
-        return MyPageGroupOthersProviderSerializer(obj.provider).data
+        return ProviderSerializer(group.provider).data
+
+
+class MyPageGroupSerializer(serializers.ModelSerializer):
+    """Group serailizer in mypage format"""
+
+    default = MyPageGroupDefaultSerializer()
+    others = MyPageGroupOthersSerializer(many=True, read_only=True)
+
+    class Meta:
+        """Metadata of MyPageGroupSerializer"""
+
+        model = Group
+        read_only_fields = "__all__"
