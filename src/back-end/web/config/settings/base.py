23e1f3474e5f4e7462b2d664e7dfdba588243252
Dahee Goo <dadaahee@gmail.com>
2022-05-11 02:53:14 +0900

    feat: djangorestframework-camel-case 설정 추가

--- src/back-end/web/config/settings/base.py
+++ src/back-end/web/config/settings/base.py
@@ -85,6 +85,19 @@ INSTALLED_APPS = [
 ] + CUSTOM_APPS
 
 REST_FRAMEWORK = {
+    # camel case converter
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+    ),
+    "JSON_UNDERSCOREIZE": {
+        "no_underscore_before_number": True,
+    },
     # API document automation: drf-spectacular
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     # Permit only to authenticated user
