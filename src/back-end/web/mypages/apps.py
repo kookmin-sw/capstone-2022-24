d65e313b2289d3afba22532c4efb35f02a1c7bab
Dahee Goo <dadaahee@gmail.com>
2022-05-11 05:03:24 +0900

    feat: 마이페이지 어플리케이션 추가

---
+++ src/back-end/web/mypages/apps.py
@@ -0,0 +1,9 @@
+"""Configurations of mypage application"""
+from django.apps import AppConfig
+
+
+class MypagesConfig(AppConfig):
+    """Configuration details of mypage application"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mypages"
---
+++ src/back-end/web/mypages/views.py
@@ -0,0 +1,1 @@
+"""APIs of mypages application"""
