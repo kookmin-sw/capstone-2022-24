b8f4b27c5659fcf6dbff8eff0ed08bd11c45912a
Dahee Goo <dadaahee@gmail.com>
2022-05-13 09:22:53 +0900

    update: queryset 용어 정리, view 구조 단순화

--- src/back-end/web/mypages/views.py
+++ src/back-end/web/mypages/views.py
@@ -5,7 +5,6 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.models import User
-from users.serializers import UserSerializer
 
 
 @extend_schema(tags=["Priority-1", "User"], operation_id="사용자 상세 정보(마이페이지) 조회")
@@ -15,13 +14,13 @@ class MyPageDetailView(APIView):
     사용자 정보 조회 (사용자/참여중인 모임/최근 조회작/관람작/찜작 - 첫페이지 작품까지 포함)
     """
 
-    user_queryset = User.objects.all()
+    queryset = User.objects.all()
     serializer_class = MyPageSerializer
 
     def get_user(self):
         """Get request user"""
         # get login user
-        obj = get_object_or_404(self.user_queryset, id=self.request.user.id)
+        obj = get_object_or_404(self.queryset, id=self.request.user.id)
         # raise permission error if user is invalid
         self.check_object_permissions(self.request, obj)
         return obj
@@ -29,55 +28,5 @@ class MyPageDetailView(APIView):
     def get(self, request, *args, **kwargs):
         """GET /users/mypage/"""
         user = self.get_user()
-        # serializer = self.serializer_class(user)
-
-        # get mypage data
-        profile = UserSerializer(user).data
-        groups = {
-            "default": {
-                "provider": {},
-                "status": None,
-                "time_stamps": None,
-                "report": {
-                    "reported": None,
-                    "report_count": None,
-                    "leader_report_count": None,
-                },
-                "account": None,
-                "fellows": [{"id": None}],
-            },
-            "others": [
-                {"provider": {"id": None, "nickname": None, "link": None, "logo_url": None}},
-            ],
-        }
-        videos = {
-            "recent_views": {
-                "results": [{"id": None, "tmdb_id": None, "poster_url": None}],
-                "page": {
-                    "total_page": None,
-                    "total_result": None,
-                },
-            },
-            "watch_marks": {
-                "results": [{"id": None, "tmdb_id": None, "poster_url": None}],
-                "page": {
-                    "total_page": None,
-                    "total_result": None,
-                },
-            },
-            "wishes": {
-                "results": [],
-                "page": {
-                    "total_page": None,
-                    "total_result": None,
-                },
-            },
-            "stars": {
-                "results": [],
-                "page": {
-                    "total_page": None,
-                    "total_result": None,
-                },
-            },
-        }
-        return Response(data={"profile": profile, "groups": groups, "videos": videos})
+        serializer = self.serializer_class(user)
+        return Response(serializer.data)
