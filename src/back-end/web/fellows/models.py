3a2400a795586b01b6c6dae4a134b14f528726da
Dahee Goo <dadaahee@gmail.com>
2022-05-14 02:43:57 +0900

    fix: leader 객체 생성 오류 수정

--- src/back-end/web/fellows/models.py
+++ src/back-end/web/fellows/models.py
@@ -56,8 +56,5 @@ class Leader(models.Model):
 
         db_table = "leader"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-
     def __str__(self):
         return f"모임장 #{self.id}"
