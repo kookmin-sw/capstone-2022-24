aa5aa9e539aa396afd74715fe0a7a79b5a72ba4f
Dahee Goo <dadaahee@gmail.com>
2022-05-13 09:20:39 +0900

    udpate: 추상 모델은 시리얼라이저x -> 일반 Serializer 상속으로 변환, default status 부여

--- src/back-end/web/applies/serializers.py
+++ src/back-end/web/applies/serializers.py
@@ -1,32 +1,25 @@
 """Serializers of applies application for json parsing"""
-from applies.models import BaseApply
-from payments.serializers import PaymentSerializer
 from providers.serializers import ProviderSerializer
 from rest_framework import serializers
-from users.serializers import UserSerializer
 
 
-class BaseApplySerializer(serializers.ModelSerializer):
+class BaseApplySerializer(serializers.Serializer):
     """Abstract apply model serializer"""
 
-    user = serializers.SerializerMethodField()
-    payment = serializers.SerializerMethodField()
+    def update(self, instance, validated_data):
+        """Not used"""
+
+    def create(self, validated_data):
+        """Not used"""
+
     provider = serializers.SerializerMethodField()
-
-    class Meta:
-        """Metadata of BaseApplySerializer"""
-
-        model = BaseApply
-        fields = "__all__"
-
-    def get_user(self, obj):
-        """Get user data using UserSerializer"""
-        return UserSerializer(obj.user).data
-
-    def get_payment(self, obj):
-        """Get payment data using PaymentSerializer"""
-        return PaymentSerializer(obj.payment).data
+    apply_date_time = serializers.DateTimeField()
+    status = serializers.SerializerMethodField()
 
     def get_provider(self, obj):
         """Get provider data using ProviderSerializer"""
         return ProviderSerializer(obj.provider).data
+
+    def get_status(self, obj):
+        """Get default group status - <Recruiting>"""
+        return "Recruiting"
