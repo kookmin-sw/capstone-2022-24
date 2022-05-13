2f5874bfc37eca3395315110146b7672d6c748c2
Dahee Goo <dadaahee@gmail.com>
2022-05-14 05:41:00 +0900

    feat: 페이지네이션 클래스 추가

--- src/back-end/web/mypages/pagination.py
+++ src/back-end/web/mypages/pagination.py
@@ -1,8 +1,37 @@
 """Pagination classes to separate total video list"""
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
-class MyVideoPagination(LimitOffsetPagination):
+class VideoHistoryPagination(LimitOffsetPagination):
     """Video pagenation used in mypage"""
 
     default_limit = 24
+    limit_query_param = "limit"
+    offset_query_param = "offset"
+
+    def get_paginated_response(self, data):
+        """Make paginated response with data"""
+        return Response(
+            {
+                "page": {
+                    "limit": self.page.paginator.limit,
+                    "offset": self.page.paginator.offset,
+                    "total_count": self.page.paginator.count,
+                },
+                "count": self.page.paginator.count,
+                "results": data,
+            }
+        )
+
+    def get_paginated_result(self, data):
+        """Make paginated result(not response!) with data"""
+        return {
+            "page": {
+                "limit": self.page.paginator.limit,
+                "offset": self.page.paginator.offset,
+                "total_count": self.page.paginator.count,
+            },
+            "count": self.page.paginator.count,
+            "results": data,
+        }
