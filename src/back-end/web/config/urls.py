3feeb3f36e1c21ad8daa04bd342275c0be746141
Dahee Goo <dadaahee@gmail.com>
2022-05-16 19:13:56 +0900

    chore: debug 툴바 중복 제거

--- src/back-end/web/config/urls.py
+++ src/back-end/web/config/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
     path("groups/", include("groups.urls")),
     path("users/", include("users.urls")),
     path("users/mypage/", include("mypages.urls")),
-    # debug
-    path("__debug__/", include("debug_toolbar.urls")),
     # api docs
     path("docs/schema/", SpectacularJSONAPIView.as_view(), name="docs"),  # api documentation file
     path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="docs"), name="swagger"),  # api docs by swagger
