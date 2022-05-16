b19947c7bb754c880627dec1bcc710ac9900ad49
Dahee Goo <dadaahee@gmail.com>
2022-05-17 00:50:57 +0900

    update: 작품 기록 관련 페이지네이션 예제 추가

--- src/back-end/web/mypages/schemas.py
+++ src/back-end/web/mypages/schemas.py
@@ -47,16 +47,40 @@ MYPAGE_DETAIL_EXAMPLES = [
                     ],
                 },
                 "videos": {
-                    "recentViews": [],
-                    "watchMarks": [],
-                    "wishes": [
-                        {
-                            "id": 40,
-                            "tmdbId": 19995,
-                            "posterUrl": "https://image.tmdb.org/t/p/original/zygmx5abXeDpr3fWYX4jlXFZ1wh.jpg",
-                        }
-                    ],
-                    "stars": [],
+                    "recentViews": {
+                        "page": {"limit": 3, "offset": 0, "totalCount": 4},
+                        "count": 4,
+                        "results": [
+                            {
+                                "id": 9,
+                                "tmdbId": 454626,
+                                "posterUrl": "https://image.tmdb.org/t/p/original/pMXOlasWr1IzHGH8HWw1ZTXs6rQ.jpg",
+                            },
+                            {
+                                "id": 14,
+                                "tmdbId": 783461,
+                                "posterUrl": "https://image.tmdb.org/t/p/original/ppn4ZO8qmylxRwFjfBWPkmMRdSs.jpg",
+                            },
+                            {
+                                "id": 19,
+                                "tmdbId": 800407,
+                                "posterUrl": "https://image.tmdb.org/t/p/original/hmqocSNKCgMY5yrVOOmfCUHdXkl.jpg",
+                            },
+                        ],
+                    },
+                    "watchMarks": {"page": {"limit": 3, "offset": 0, "totalCount": 0}, "count": 0, "results": []},
+                    "wishes": {
+                        "page": {"limit": 3, "offset": 0, "totalCount": 1},
+                        "count": 1,
+                        "results": [
+                            {
+                                "id": 167,
+                                "tmdbId": 76479,
+                                "posterUrl": "https://image.tmdb.org/t/p/original/dzOxNbbz1liFzHU1IPvdgUR647b.jpg",
+                            }
+                        ],
+                    },
+                    "stars": {"page": {"limit": 3, "offset": 0, "totalCount": 0}, "count": 0, "results": []},
                 },
             }
         ],
@@ -65,25 +89,95 @@ MYPAGE_DETAIL_EXAMPLES = [
         response_only=True,
         name="Success Example",
         value={
-            "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
-            ".eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUyMjE3Mjg0LCJpYXQiOjE2NTIyMTAwODQsImp0aSI6IjA0ODY3"
-            "NjA2NTIzMzRmMDhiN2Q3MTc2NjQ0NTg3YjI3IiwidXNlcl9pZCI6Mjl9.CDOzt"
-            "k-o6jvEbfdzAouTAgIypzFgAcm4jbRzzGeDais",
-            "refreshToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
-            ".eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY1MjIzMTY4NCwiaW"
-            "F0IjoxNjUyMjEwMDg0LCJqdGkiOiIwNjUxZjdlNGJlMDY0ZjEzYWY5MjU2ZGIyN"
-            "GVlNTVkNiIsInVzZXJfaWQiOjI5fQ.ael6_7GMvE39X6qESW__gikrLrbQAeYTtAllWmN_sYc",
-            "user": {
-                "nickname": "Ongot",
+            "profile": {
+                "nickname": "ongot",
                 "name": "ongot",
                 "email": "ongot@123.com",
-                "cellPhoneNumber": "010-1111-1111",
-                "profileImageUrl": "https://upload.wikimedia.org/wikipedia/"
-                "commons/thumb/a/ab/Swagger-logo.png/150px-Swagger"
-                "-logo.png",
-                "birthday": "1998-01-01",
+                "cellPhoneNumber": "010-1234-5678",
+                "profileImageUrl": None,
+                "birthday": "1990-01-01",
                 "isActive": True,
                 "isVerified": True,
+            },
+            "groups": {
+                "default": {
+                    "provider": {
+                        "id": 3,
+                        "tmdbId": 356,
+                        "name": "WC",
+                        "link": "",
+                        "logoUrl": "https://some.storage.com/providers/logo_images/app/back-end/static/https://im"
+                        "age.tmdb.org/t/p/original/cNi4Nv5EPsnvf5WmgwhfWDsdMUd.jpg",
+                    },
+                    "status": "Recruiting",
+                    "timeStamps": {
+                        "creationDateTime": "2022-05-16T20:14:17",
+                        "startWatchingDateTime": "2022-05-16T20:14:31",
+                        "endWatchingDateTime": None,
+                        "endReportingDateTime": "2022-05-19T20:14:31",
+                    },
+                    "fellows": [
+                        {"id": 1, "nickname": "ongot", "profileImageUrl": None, "isLeader": True, "isMyself": True},
+                        {
+                            "id": 2,
+                            "nickname": "캡스톤",
+                            "profileImageUrl": "https://shop3.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop"
+                            "3.daumcdn.net%2Fshophow%2Fp%2FS16182251276.jpg%3Fut%3D20220129023802",
+                            "isLeader": True,
+                            "isMyself": False,
+                        },
+                    ],
+                    "account": {
+                        "id": 1,
+                        "identifier": None,
+                        "password": None,
+                        "creationDateTime": None,
+                        "lastModificationDateTime": None,
+                    },
+                    "report": {"reported": False, "reportCount": 0, "leaderReportCount": 0},
+                },
+                "others": [],
+            },
+            "videos": {
+                "recentViews": {
+                    "page": {"limit": 5, "offset": 0, "totalCount": 4},
+                    "count": 4,
+                    "results": [
+                        {
+                            "id": 9,
+                            "tmdbId": 454626,
+                            "posterUrl": "https://image.tmdb.org/t/p/original/pMXOlasWr1IzHGH8HWw1ZTXs6rQ.jpg",
+                        },
+                        {
+                            "id": 14,
+                            "tmdbId": 783461,
+                            "posterUrl": "https://image.tmdb.org/t/p/original/ppn4ZO8qmylxRwFjfBWPkmMRdSs.jpg",
+                        },
+                        {
+                            "id": 19,
+                            "tmdbId": 800407,
+                            "posterUrl": "https://image.tmdb.org/t/p/original/hmqocSNKCgMY5yrVOOmfCUHdXkl.jpg",
+                        },
+                        {
+                            "id": 38,
+                            "tmdbId": 166426,
+                            "posterUrl": "https://image.tmdb.org/t/p/original/arcIFL50IPQT7JDG2SD6tzcOlRL.jpg",
+                        },
+                    ],
+                },
+                "watchMarks": {"page": {"limit": 5, "offset": 0, "totalCount": 0}, "count": 0, "results": []},
+                "wishes": {
+                    "page": {"limit": 5, "offset": 0, "totalCount": 1},
+                    "count": 1,
+                    "results": [
+                        {
+                            "id": 167,
+                            "tmdbId": 76479,
+                            "posterUrl": "https://image.tmdb.org/t/p/original/dzOxNbbz1liFzHU1IPvdgUR647b.jpg",
+                        }
+                    ],
+                },
+                "stars": {"page": {"limit": 5, "offset": 0, "totalCount": 0}, "count": 0, "results": []},
             },
         },
     ),
