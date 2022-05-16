6af92bbf436b971c25b57a8ba6de85bf10f7b399
Dahee Goo <dadaahee@gmail.com>
2022-05-17 03:54:00 +0900

    feat: WishView(찜한 작품 조회 API) 구현 및 urls.py 추가

--- src/back-end/web/wishes/urls.py
+++ src/back-end/web/wishes/urls.py
@@ -1,3 +1,7 @@
 """wishes URL Configuration"""
+from django.urls import path
+from wishes.views import WishView
 
-urlpatterns = []
+urlpatterns = [
+    path("", WishView.as_view({"get": "list"}), name="users_mypage_wishes"),
+]
--- src/back-end/web/wishes/views.py
+++ src/back-end/web/wishes/views.py
@@ -1,1 +1,27 @@
 """APIs of wishes application"""
+from config.exceptions.input import InvalidPaginationParameterException
+from mypages.pagination import VideoHistoryPagination
+from rest_framework import viewsets
+from wishes.models import Wish
+from wishes.serializers import WishSerializer
+
+
+# pylint: disable=R0901
+class WishView(viewsets.ModelViewSet):
+    """Wish apis of user's video history"""
+
+    queryset = Wish.objects.select_related("user", "video")
+    serializer_class = WishSerializer
+    pagination_class = VideoHistoryPagination
+
+    def get_queryset(self):
+        """get login user's wish objects"""
+        # (required) query parameters: videoLimit & videoOffset (used in pagination)
+        if VideoHistoryPagination.limit_query_param not in self.request.query_params:
+            raise InvalidPaginationParameterException()
+        if VideoHistoryPagination.offset_query_param not in self.request.query_params:
+            raise InvalidPaginationParameterException()
+        # other query parameters are included
+        if len(self.request.query_params) >= 3:
+            raise InvalidPaginationParameterException()
+        return self.request.user.wish_set.all()
