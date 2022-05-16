5b6335cafcf3a5c7f7f6435b6577cd95215459a8
Dahee Goo <dadaahee@gmail.com>
2022-05-17 03:50:09 +0900

    feat: 페이지네이션 Query Parameter 관련 Exception 추가

--- src/back-end/web/config/exceptions/input.py
+++ src/back-end/web/config/exceptions/input.py
@@ -8,3 +8,11 @@ class BadFormatException(APIException):
     status_code = 400
     default_detail = "올바르지 않은 형식입니다."
     default_code = "Bad Request Key"
+
+
+class InvalidPaginationParameterException(APIException):
+    """Query parameter(s) related to pagination is(are) not allowed or required query parameter(s) is(are) missing"""
+
+    status_code = 400
+    default_detail = "페이지 요청 형식이 올바르지 않습니다."
+    default_code = "invalid Pagination"
