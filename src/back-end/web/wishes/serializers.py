c29f1e6aed4de27cd8a0be4dc0a88cc175808ae9
Dahee Goo <dadaahee@gmail.com>
2022-05-17 03:52:13 +0900

    update: WishSerializer 단일 depth로 변경, poster_url로 수정

--- src/back-end/web/wishes/serializers.py
+++ src/back-end/web/wishes/serializers.py
@@ -1,27 +1,24 @@
 """Serializers of wish application for json parsing"""
 from rest_framework import serializers
-from users.serializers import UserSerializer
-from videos.serializers import VideoSerializer
 from wishes.models import Wish
 
 
 class WishSerializer(serializers.ModelSerializer):
     """Serializer of Wish model"""
 
-    user = serializers.SerializerMethodField()
-    video = serializers.SerializerMethodField()
+    id = serializers.IntegerField(source="video.id")
+    tmdb_id = serializers.IntegerField(source="video.tmdb_id")
+    title = serializers.CharField(max_length=200, source="video.title")
+    poster_url = serializers.URLField(source="video.poster_key", allow_null=True)
 
     class Meta:
         """Metadata of Wish serializer"""
 
         model = Wish
-        fields = "__all__"
-        read_only_fields = "__all__"
-
-    def get_user(self, obj):
-        """Get user data using UserSerializer"""
-        return UserSerializer(obj.user).data
-
-    def get_video(self, obj):
-        """Get video data using UserSerializer"""
-        return VideoSerializer(obj.video).data
+        fields = [
+            "id",
+            "tmdb_id",
+            "title",
+            "poster_url",
+            "date_time",
+        ]
