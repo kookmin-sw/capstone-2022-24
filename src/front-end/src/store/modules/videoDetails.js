d9479520b07f2a80e5f0181e27e056c37a4615e0
Asung <a.city521@gmail.com>
2022-05-16 20:35:56 +0900

    feat: 영화 상세 페이지

---
+++ src/front-end/src/store/modules/videoDetails.js
@@ -0,0 +1,28 @@
+import http from '@/api/http';
+
+export const videoDetails = {
+	namespaced: true,
+	state: {
+		videoDetails: {},
+	},
+	getters: {},
+	mutations: {
+		SET_VIDEO_DETAILS(state, details) {
+			state.videoDetails = details;
+		},
+	},
+	actions: {
+		async loadVideoDetails({ commit }, video) {
+			const url = `/videos/${video.category}/${video.videoId}`;
+			await http
+				.get(url)
+				.then(res => {
+					const details = res.data;
+					commit('SET_VIDEO_DETAILS', details);
+				})
+				.catch(() => {
+					console.log('');
+				});
+		},
+	},
+};
