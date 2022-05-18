1419b2e8b666c801c9255e5a2060dadef4bec2f9
Dahee Goo <dadaahee@gmail.com>
2022-05-19 03:43:17 +0900

    update: 모델 unique 디테일 설정 추가, dibs_count -> wish_count 변경, data.json 스웨거 샘플 유저 추가

--- src/back-end/web/video_total_counts/models.py
+++ src/back-end/web/video_total_counts/models.py
@@ -7,11 +7,13 @@ from videos.models import Video
 class VideoTotalCount(models.Model):
     """Definition of video total count information about interacting with Users"""
 
+    MAX_COUNT_LIMIT = 2147483647
+
     video = models.OneToOneField(
         Video,
         on_delete=models.CASCADE,
     )
-    dibs_count = models.PositiveIntegerField(
+    wish_count = models.PositiveIntegerField(
         null=True,
         default=0,
     )
@@ -31,3 +33,33 @@ class VideoTotalCount(models.Model):
 
     def __str__(self):
         return f"{self.video.title}의 total Counts"
+
+    def increase_wish_count(self):
+        """Increase wish count"""
+        if self.wish_count < self.MAX_COUNT_LIMIT:
+            self.wish_count += 1
+
+    def decrease_wish_count(self):
+        """Decrease wish count"""
+        if self.wish_count > 0:
+            self.wish_count -= 1
+
+    def increase_watch_count(self):
+        """Increase wish count"""
+        if self.watch_count < self.MAX_COUNT_LIMIT:
+            self.watch_count += 1
+
+    def decrease_watch_count(self):
+        """Decrease wish count"""
+        if self.watch_count > 0:
+            self.watch_count -= 1
+
+    def increase_view_count(self):
+        """Increase wish count"""
+        if self.view_count < self.MAX_COUNT_LIMIT:
+            self.view_count += 1
+
+    def decrease_view_count(self):
+        """Decrease wish count"""
+        if self.view_count > 0:
+            self.view_count -= 1
