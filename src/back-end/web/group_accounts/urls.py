a44f1a951adaf60408e5ec3fc15349c6fa655f30
Dahee Goo <dadaahee@gmail.com>
2022-05-19 06:17:07 +0900

    feat: 모임 계정 등록/삭제 url 추가

--- src/back-end/web/group_accounts/urls.py
+++ src/back-end/web/group_accounts/urls.py
@@ -1,3 +1,8 @@
 """group_accounts URL Configuration"""
+from django.urls import path
+from group_accounts.views import GroupAccountIDView, GroupAccountPWView
 
-urlpatterns = []
+urlpatterns = [
+    path("<int:group_id>/account/id/", GroupAccountIDView.as_view(), name="groups_account_id"),
+    path("<int:group_id>/account/password/", GroupAccountPWView.as_view(), name="groups_account_pw"),
+]
