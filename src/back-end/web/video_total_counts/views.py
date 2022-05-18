0d28a0cd950048be9ed2470a651fa7a91376375e
Dahee Goo <dadaahee@gmail.com>
2022-05-19 05:39:21 +0900

    fix: wish_count 갱신 관련 오타 수정

--- src/back-end/web/video_total_counts/views.py
+++ src/back-end/web/video_total_counts/views.py
@@ -12,5 +12,5 @@ def increase_wish_count(video):
 def decrease_wish_count(video):
     """Decrease wish_count of video's total count object"""
     _counter = video.videototalcount  # type: VideoTotalCount
-    _counter.increase_wish_count()
+    _counter.decrease_wish_count()
     _counter.save()
