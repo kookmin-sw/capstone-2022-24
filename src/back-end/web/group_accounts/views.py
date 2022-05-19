36cb0e4b26a3723c369fb1fe4481e4fc5cba791b
Dahee Goo <dadaahee@gmail.com>
2022-05-20 01:05:22 +0900

    feat: 모임 계정 변경 시 상태 변경 기능 추가, 예외 처리

--- src/back-end/web/group_accounts/views.py
+++ src/back-end/web/group_accounts/views.py
@@ -1,11 +1,13 @@
 """APIs of group_accounts application"""
 from drf_spectacular.utils import extend_schema, extend_schema_view
+from group_accounts.exceptions import CanNotRegisterGroupAccountException
 from group_accounts.models import GroupAccount
 from group_accounts.serializers import (
     GroupAccountIDSerializer,
     GroupAccountPWSerializer,
 )
-from groups.models import Group
+from groups.views import can_start_watch, start_watch
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -14,12 +16,32 @@ class GroupAccountView(UpdateAPIView):
     """Base group account id/pw view"""
 
     queryset = GroupAccount.objects.prefetch_related(
-        "group", "group__provider" "group__fellow_set", "group__fellow_set__user", "group__fellow_set__leader"
-    )  # type: QueryDict[Group]
+        "group", "group__provider", "group__fellow_set", "group__fellow_set__user", "group__fellow_set__leader"
+    )  # type: QueryDict[GroupAccount]
     permission_classes = (IsAuthenticated,)  # TODO: IsLeaderOrFellowReadOnly
     lookup_field = "group__id"
     lookup_url_kwarg = "group_id"
     http_method_names = ["patch"]
+
+    def patch(self, request, *args, **kwargs):
+        """PATCH /groups/{group_id}/account/id"""
+        try:
+            _account = self.get_object()  # type: GroupAccount
+            _group = _account.group
+            # 모임의 계정 변경 가능 여부 확인
+            # (1. 리더 여부 & 권한 확인 - permission_classes에서 확인)
+            # 2. 모임 상태 확인: 모집 완료 맞음?
+            if not _group.can_register_account:
+                raise CanNotRegisterGroupAccountException
+            # 모임 계정 변경
+            response = super().patch(request, *args, **kwargs)
+            # 모임 상태 변경 필요? (모집완료 -> 관람중~)
+            if can_start_watch(_group):
+                # 모임 상태 변경
+                start_watch(_group)
+            return response
+        except ValidationError as error:
+            raise error
 
 
 @extend_schema_view(
