83614072f09d5821b6b4f100f38f1c54b9d5418e
Dahee Goo <dadaahee@gmail.com>
2022-05-20 07:10:02 +0900

    docs: api 문서용 providers 조회 예시 추가

---
+++ src/back-end/web/providers/schemas.py
@@ -0,0 +1,21 @@
+"""Example schemas in providers application"""
+
+PROVIDER_LIST_EXAMPLES = [
+    {
+        "appliedProviders": [
+            {
+                "id": 5,
+                "name": "AP",
+                "logoUrl": "https://image.tmdb.org/t/p/original/68MNrwlkpF7WnmNPXLah69CR5cb.jpg",
+                "link": "https://www.primevideo.com/ref=atv_nb_logo?_encoding=UTF8&language=ko_KR",
+            },
+            {
+                "id": 6,
+                "name": "LF",
+                "logoUrl": "https://blog.kakaocdn.net/dn/beMRoh/btqJUNc2uBT/gLb6LDCKEemdV8IEki0St0/img.png",
+                "link": "https://laftel.net/",
+            },
+        ],
+        "notAppliedProviders": [],
+    }
+]
