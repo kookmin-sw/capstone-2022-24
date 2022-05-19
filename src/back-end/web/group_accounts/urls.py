4e0f1334a2bbf68f97bd0e2e26eb04b59ee7b052
Dahee Goo <dadaahee@gmail.com>
2022-05-19 22:47:35 +0900

    update: 모임 계정 urls.py 간략하게 변경

--- src/back-end/web/group_accounts/urls.py
+++ src/back-end/web/group_accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from group_accounts.views import GroupAccountIDView, GroupAccountPWView
 
 urlpatterns = [
-    path("<int:group_id>/account/id/", GroupAccountIDView.as_view(), name="groups_account_id"),
-    path("<int:group_id>/account/password/", GroupAccountPWView.as_view(), name="groups_account_pw"),
+    path("id/", GroupAccountIDView.as_view(), name="groups_account_id"),
+    path("password/", GroupAccountPWView.as_view(), name="groups_account_pw"),
 ]
