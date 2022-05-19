e9e14f0564ffcc3c4dcb29484ed869bd9666261d
Dahee Goo <dadaahee@gmail.com>
2022-05-20 07:01:31 +0900

    refactor: 모임 조회 api endpoint 변경

--- src/back-end/web/groups/urls.py
+++ src/back-end/web/groups/urls.py
@@ -1,7 +1,8 @@
 """groups URL Configuration"""
 from django.urls import path
-from groups.views import GruopPaymentView
+from groups.views import GroupDetailView, GruopPaymentView
 
 urlpatterns = [
     path("payments/", GruopPaymentView.as_view({"post": "payment"}), name="group payments"),
+    path("<int:group_id>/", GroupDetailView.as_view(), name="groups_detail"),
 ]
