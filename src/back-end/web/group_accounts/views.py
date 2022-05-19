6c411116be630ff038d18fd16933ce7dd437cebe
Dahee Goo <dadaahee@gmail.com>
2022-05-19 22:48:04 +0900

    docs: API 문서 관련 설명 추가

--- src/back-end/web/group_accounts/views.py
+++ src/back-end/web/group_accounts/views.py
@@ -1,4 +1,5 @@
 """APIs of group_accounts application"""
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from group_accounts.models import GroupAccount
 from group_accounts.serializers import (
     GroupAccountIDSerializer,
@@ -15,17 +16,32 @@ class GroupAccountView(UpdateAPIView):
     queryset = GroupAccount.objects.prefetch_related(
         "group", "group__provider" "group__fellow_set", "group__fellow_set__user", "group__fellow_set__leader"
     )  # type: QueryDict[Group]
-    permission_classes = [IsAuthenticated]  # TODO: IsLeaderOrFellowReadOnly
+    permission_classes = (IsAuthenticated,)  # TODO: IsLeaderOrFellowReadOnly
     lookup_field = "group__id"
     lookup_url_kwarg = "group_id"
+    http_method_names = ["patch"]
 
 
+@extend_schema_view(
+    operation_id="OTT 계정 ID 변경",
+    patch=extend_schema(
+        tags=["Priority-1", "Group"],
+        operation_id="OTT 계정 ID 변경",
+        description="Set identifier of the group account by leader",
+    ),
+)
 class GroupAccountIDView(GroupAccountView):
     """PATCH /groups/{group_id}/accounts/id: Set Group Account ID"""
 
     serializer_class = GroupAccountIDSerializer
 
 
+@extend_schema_view(
+    operation_id="OTT 계정 PW 변경",
+    patch=extend_schema(
+        tags=["Priority-1", "Group"], operation_id="OTT 계정 PW 변경", description="Set password of the group account"
+    ),
+)
 class GroupAccountPWView(GroupAccountView):
     """PATCH /groups/{group_id}/accounts/password: Set Group Account PW"""
 
