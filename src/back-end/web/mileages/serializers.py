5c132d53436221463e362d1a5272287134c90459
Dahee Goo <dadaahee@gmail.com>
2022-05-21 19:04:54 +0900

    update: patch, post 요청 시 now_total_mileages 리턴 추가, validators 추가, get 요청 시에는 now_total_mileages 삭제

--- src/back-end/web/mileages/serializers.py
+++ src/back-end/web/mileages/serializers.py
@@ -1,18 +1,33 @@
 """Definitions of serializer for mileages application"""
+from django.core.validators import MaxValueValidator, MinValueValidator
 from mileages.models import Mileage
 from rest_framework import serializers
-from users.serializers import UserSerializer
 
 
 class MileageSerializer(serializers.ModelSerializer):
     """Mileage Serializer for json parsing"""
 
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    now_total_mileages = serializers.SerializerMethodField()
+
     class Meta:
         """Meta data for Mielage serializer"""
 
         model = Mileage
-        fields = ["user", "amount", "renewal_date_time"]
+        fields = ["user", "amount", "renewal_date_time", "now_total_mileages"]
+        read_only_fields = ["renewal_date_time", "now_total_mileages"]
+        extra_kwargs = {
+            "amount": {"required": True, "validators": [MinValueValidator(-2147483647), MaxValueValidator(2147483647)]},
+            "now_total_mileages": {"validators": [MinValueValidator(0), MaxValueValidator(2147483647)]},
+        }
 
-    def get_user(self, obj):
-        """Get user data using UserSerializer"""
-        return UserSerializer(obj.user).data
+    def get_now_total_mileages(self, obj):
+        """GET login user's total mileage"""
+        return obj.user.total_mileages
+
+    def to_representation(self, instance):
+        # exclude not total mileage in getting mileage history
+        _representation = super().to_representation(instance)
+        if self.context.get("request").method == "GET":
+            _representation.pop("now_total_mileages")
+        return _representation
