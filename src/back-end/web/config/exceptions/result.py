7531e3f0ed885b59ee3ce294480cc5e2bc535e74
Dahee Goo <dadaahee@gmail.com>
2022-05-22 04:40:12 +0900

    feat: 전역 작품 기록 Not Found 예외 클래스 추가

--- src/back-end/web/config/exceptions/result.py
+++ src/back-end/web/config/exceptions/result.py
@@ -16,3 +16,11 @@ class VideoNotFoundException(APIException):
     status_code = 404
     default_detail = "존재하지 않은 작품입니다."
     default_code = "None Video"
+
+
+class VideoHistoryNotFoundException(APIException):
+    """Custom Exception for None Video"""
+
+    status_code = 404
+    default_detail = "작품 기록이 존재하지 않습니다."
+    default_code = "not_found_video_histories"
