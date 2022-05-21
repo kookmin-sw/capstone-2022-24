551b76a09a79a60152bdd138c5da8314b8624687
yesslee <lys0301174@kookmin.ac.kr>
2022-05-22 01:54:47 +0900

    design: 최신 메세지 배경색 제거, 알림 메세지 일자 변경

--- src/front-end/src/components/modals/AlarmModal.vue
+++ src/front-end/src/components/modals/AlarmModal.vue
@@ -19,14 +19,14 @@
 		<q-separator color="blue-4" />
 
 		<!--    msg 1   -->
-		<q-card-section class="text-center bg-blue-70" v-if="isActive">
+		<q-card-section class="text-center" v-if="isActive">
 			<div class="text-left q-mt-sm q-mb-sm q-ml-lg q-mr-lg">
 				<span class="q-mr-sm text-weight-bold">모임 알림</span>
 				<span>구독 종료까지 3일 남았습니다!</span>
 				<div class="text-grey q-mt-xs">
-					넷플릭스 모임 구독 종료일 - 6월 30일
+					넷플릭스 모임 구독 종료일 - 5월 25일
 				</div>
-				<div class="text-right text-grey">6월 28일</div>
+				<div class="text-right text-grey">5월 22일</div>
 			</div>
 		</q-card-section>
 		<q-separator color="grey-4" />
@@ -36,7 +36,7 @@
 				<span class="q-mr-sm text-weight-bold">결제 알림</span>
 				<span>왓챠 구독 후 계정 정보를 등록해주세요!</span>
 				<div class="text-grey q-mt-xs">왓챠 모임 결제 요청</div>
-				<div class="text-right text-grey">5월 31일</div>
+				<div class="text-right text-grey">5월 1일</div>
 			</div>
 		</q-card-section>
 		<q-separator color="grey-4" />
@@ -45,8 +45,8 @@
 			<div class="text-left q-mt-sm q-mb-sm q-ml-lg q-mr-lg">
 				<span class="q-mr-sm text-weight-bold">모임 알림</span>
 				<span>모임 모집이 완료되었습니다!</span>
-				<div class="text-grey q-mt-xs">왓챠 모임 모집 완료 - 5월 18일</div>
-				<div class="text-right text-grey">5월 18일</div>
+				<div class="text-grey q-mt-xs">왓챠 모임 모집 완료 - 4월 29일</div>
+				<div class="text-right text-grey">4월 29일</div>
 			</div>
 		</q-card-section>
 		<q-separator color="grey-4" />
@@ -56,7 +56,7 @@
 				<span class="q-mr-sm text-weight-bold">모임 알림</span>
 				<span>넷플릭스 계정 정보가 등록되었습니다!</span>
 				<div class="text-grey q-mt-xs">넷플릭스 모임 계정 정보 등록 알림</div>
-				<div class="text-right text-grey">4월 29일</div>
+				<div class="text-right text-grey">4월 27일</div>
 			</div>
 		</q-card-section>
 		<q-separator color="grey-4" />
