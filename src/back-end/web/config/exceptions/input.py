6d95c39a6c3207ecd423d6dc2f0363576f0b8077
Dahee Goo <dadaahee@gmail.com>
2022-05-23 10:16:34 +0900

    update: 각종 예외처리 추가

--- src/back-end/web/config/exceptions/input.py
+++ src/back-end/web/config/exceptions/input.py
@@ -48,3 +48,19 @@ class NotSupportedProviderException(APIException):
     status_code = 400
     default_detail = "모임을 지원하지 않는 OTT입니다."
     default_code = "not_supported_provider"
+
+
+class AlreadyJoinedGroupException(APIException):
+    """User already joined group of provider"""
+
+    status_code = 409
+    default_detail = "이미 모임에 가입한 OTT입니다."
+    default_code = "already_joined_provider_group"
+
+
+class NotEnoughSubscriptionInformationException(APIException):
+    """Not enough subscription_type, charge information about the provider (actually 500 error)"""
+
+    status_code = 404
+    default_detail = "모임 요금 정보를 찾을 수 없습니다."
+    default_code = "not_found_subscription_information"
