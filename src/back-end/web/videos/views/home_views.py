4dc4dc43cbffc4db5b6fc1fe06aa45b8aae1bad4
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-24 00:54:47 +0900

    chore: sort 로직 개선

--- src/back-end/web/videos/views/home_views.py
+++ src/back-end/web/videos/views/home_views.py
@@ -222,8 +222,7 @@ class HomeView(viewsets.ViewSet):
 
         sort = self.request.query_params.get("sort", default="random")
         try:
-            if sort:
-                queryset = queryset.order_by(self.sort_dict[sort], "id")
+            queryset = queryset.order_by(self.sort_dict[sort], "id")
         except KeyError as e:
             raise BadFormatException() from e
 
