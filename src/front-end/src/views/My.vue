04e0c553380982327f3a1eefbcfcbcb01dff1b49
yesslee <lys0301174@kookmin.ac.kr>
2022-05-25 02:16:43 +0900

    chore: 모달 import 경로 수정

--- src/front-end/src/views/My.vue
+++ src/front-end/src/views/My.vue
@@ -199,7 +199,7 @@
 <script>
 import { mapGetters, mapState } from 'vuex';
 import UserVideos from '@/components/userVideos';
-import MileageModal from '@/components/MileageModal';
+import MileageModal from '@/components/modals/MileageModal';
 import { loadTossPayments } from '@tosspayments/payment-sdk';
 
 const clientKey = 'test_ck_ADpexMgkW36nWZAzQJE3GbR5ozO0';
