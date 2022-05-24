aec0c92dad37cf55a4a9bbe066cd06744205917e
Asung <a.city521@gmail.com>
2022-05-24 21:11:04 +0900

    design: 모임 신청 완료 모달에 x버튼 삭제, 확인 버튼 추가

--- src/front-end/src/components/modals/JoinSuccessModal.vue
+++ src/front-end/src/components/modals/JoinSuccessModal.vue
@@ -1,9 +1,6 @@
 <template>
 	<q-card style="width: 600px; max-width: 80vw" v-if="isActive">
-		<q-card-section class="row items-center q-pb-none q-pt-sm">
-			<q-space />
-			<q-btn icon="close" flat round dense v-close-popup />
-		</q-card-section>
+		<q-card-section class="row items-center q-ma-sm"> </q-card-section>
 
 		<q-card-section class="col text-center q-ma-lg q-mt-none" v-if="isActive">
 			<div class="text-h6">{{ $t('join.success') }}</div>
@@ -12,6 +9,10 @@
 				<q-icon name="arrow_right_alt" size="md" />
 			</q-btn>
 		</q-card-section>
+
+		<q-card-actions align="right">
+			<q-btn flat label="확인" class="text-blue-200" v-close-popup />
+		</q-card-actions>
 	</q-card>
 </template>
 
