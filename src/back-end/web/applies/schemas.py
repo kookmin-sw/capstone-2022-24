bbebbf9ffcef47573c583515f905db90f7bc064f
Dahee Goo <dadaahee@gmail.com>
2022-05-25 02:33:44 +0900

    fix: swagger group apply 스키마 적용

--- src/back-end/web/applies/schemas.py
+++ src/back-end/web/applies/schemas.py
@@ -1,30 +1,10 @@
 """Swagger schema examples"""
 from drf_spectacular.utils import OpenApiExample
 
-GROUP_APPLY_POST_EXAMPLES = [
+GROUP_APPLY_POST_EXAMPLE = [
     OpenApiExample(
         name="Success example (201)",
         response_only=True,
-        status_codes=["POST"],
         value={"payment": {}, "providerId": 5, "applyDateTime": "2022-05-25 01:35:24"},
-    ),
-]
-
-GROUP_APPLY_PUT_EXAMPLES = [
-    OpenApiExample(
-        name="Success example (201)",
-        response_only=True,
-        status_codes=["POST"],
-        value={"payment": {}, "providerId": 5, "applyDateTime": "2022-05-25 01:35:24"},
-    ),
-]
-
-GROUP_APPLY_REQUEST_EXAMPLES = [
-    OpenApiExample(
-        name="Request Example",
-        request_only=True,
-        value={
-            "providerId": 5,
-        },
     ),
 ]
