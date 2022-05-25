924656f048fa97ce4632908f13317e6147d3cf55
Asung <a.city521@gmail.com>
2022-05-25 16:55:27 +0900

    chore: mock data 구조 수정

--- src/front-end/src/mocks/handlers/oauth.js
+++ src/front-end/src/mocks/handlers/oauth.js
@@ -1,6 +1,33 @@
 import { rest } from 'msw';
 
 export default [
+	// 회원가입 테스트용 임의의 엔드포인트
+	rest.post('/login/', (req, res, ctx) => {
+		return res(
+			ctx.status(200),
+			ctx.set({
+				Location: 'https://localhost:80/register',
+			}),
+			ctx.json({
+				accessToken: 'ongot.access.token',
+				refreshToken: 'ongot.refresh.token',
+				user: {
+					user: {
+						nickname: 'ongot',
+						name: 'ongot',
+						email: 'ongot@123.com',
+						cellPhoneNumber: '010-1111-1111',
+						profileImageUrl:
+							'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Swagger-logo.png/150px-Swagger-logo.png',
+						birthday: '1998-01-01',
+						isActive: true,
+						isVerified: true,
+					},
+					providers: {},
+				},
+			}),
+		);
+	}),
 	// 네이버 로그인 mock data : status 200 (로그인 성공)
 	rest.post('/users/login/oauth/naver/', (req, res, ctx) => {
 		return res(
