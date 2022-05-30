48c50e5ebd75daf3243e56befe2d160e9a300e14
Asung <a.city521@gmail.com>
2022-05-30 10:37:51 +0900

    style: 그룹 상세 정보 목 데이터 생성

--- src/front-end/src/mocks/handlers/groups.js
+++ src/front-end/src/mocks/handlers/groups.js
@@ -1,76 +1,106 @@
 import { rest } from 'msw';
 
 export default [
-	rest.get('/users/providers/ott0002', (req, res, ctx) => {
+	rest.get('/providers/1', (req, res, ctx) => {
 		return res(
 			ctx.status(200),
 			ctx.json({
+				id: 1,
 				provider: {
-					id: 'ott0002',
-					name: '왓챠',
-					link: 'https://watcha.com/',
-					logoUrl: 'watcha logo img',
+					id: 5,
+					tmdbId: 119,
+					name: '아마존 프라임',
+					link: 'https://www.primevideo.com/offers/nonprimehomepage/ref=atv_nb_lcl_ko_KR',
+					logoUrl:
+						'https://image.tmdb.org/t/p/original/68MNrwlkpF7WnmNPXLah69CR5cb.jpg',
 				},
-				status: 'watching',
+				status: 'Recruiting',
+				account: {
+					id: 1,
+					identifier: null,
+					password: null,
+					creationDateTime: null,
+					lastModificationDateTime: null,
+				},
 				timeStamps: {
-					creationDateTime: '2022-03-12',
-					startWatchingDateTime: '2022-03-24',
-					endWatchingDateTime: '2022-04-23',
-					endReportingDateTime: '2022-03-27',
+					creationDateTime: '2022-05-20 02:33:15',
+					startWatchingDateTime: null,
+					endWatchingDateTime: null,
+					endReportingDateTime: null,
 				},
+				fellows: [],
 				report: {
 					reported: false,
 					reportCount: 0,
+					leaderReportCount: 0,
 				},
-				account: {
-					id: 'watcha@email.com',
-					password: 'watcha02pW',
-					lastModifiedDateTime: '2022-03-24',
-				},
-				fellows: [
-					{
-						id: 'user0001',
-						profileImgUrl: '01ImgUrl',
-						nickname: '모르는개산책',
-						isLeader: false,
-						isMyself: true,
-					},
-					{
-						id: 'user0005',
-						profileImgUrl: '05ImgUrl',
-						nickname: '왜전부있는닉이래',
-						isLeader: true,
-						isMyself: false,
-					},
-					{
-						id: 'user0006',
-						profileImgUrl: '06ImgUrl',
-						nickname: 'bb',
-						isLeader: false,
-						isMyself: false,
-					},
-					{
-						id: 'user0007',
-						profileImgUrl: '07ImgUrl',
-						nickname: 'SIM',
-						isLeader: false,
-						isMyself: false,
-					},
-				],
+				role: null,
 			}),
 		);
 	}),
-	rest.get('/users/providers/ott0003', (req, res, ctx) => {
+	rest.get('/providers/6', (req, res, ctx) => {
 		return res(
 			ctx.status(200),
 			ctx.json({
+				id: 2,
 				provider: {
-					id: 'ott0003',
-					name: 'wavve',
-					link: 'https://www.wavve.com/',
-					logoUrl: 'wavve logo img',
+					id: 6,
+					tmdbId: 0,
+					name: '라프텔',
+					link: 'https://laftel.net/',
+					logoUrl:
+						'https://blog.kakaocdn.net/dn/beMRoh/btqJUNc2uBT/gLb6LDCKEemdV8IEki0St0/img.png',
 				},
-				status: 'recruiting',
+				status: 'Recruited',
+				account: {
+					id: 2,
+					identifier: null,
+					password: null,
+					creationDateTime: null,
+					lastModificationDateTime: null,
+				},
+				timeStamps: {
+					creationDateTime: '2022-05-20 02:33:25',
+					startWatchingDateTime: null,
+					endWatchingDateTime: null,
+					endReportingDateTime: null,
+				},
+				fellows: [
+					{
+						id: 4,
+						nickname: 'string',
+						profileImageUrl: null,
+						isLeader: false,
+						isMyself: false,
+					},
+					{
+						id: 3,
+						nickname: '본인',
+						profileImageUrl: null,
+						isLeader: true,
+						isMyself: true,
+					},
+					{
+						id: 2,
+						nickname: '캡스톤',
+						profileImageUrl:
+							'https://shop3.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop3.daumcdn.net%2Fshophow%2Fp%2FS16182251276.jpg%3Fut%3D20220129023802',
+						isLeader: false,
+						isMyself: false,
+					},
+					{
+						id: 1,
+						nickname: 'ongot',
+						profileImageUrl: null,
+						isLeader: true,
+						isMyself: true,
+					},
+				],
+				report: {
+					reported: false,
+					reportCount: 0,
+					leaderReportCount: 0,
+				},
 			}),
 		);
 	}),
