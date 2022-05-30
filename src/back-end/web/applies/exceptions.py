635ca8eecf7a271efd77861ff7e5b9de0e16d19a
Dahee Goo <dadaahee@gmail.com>
2022-05-31 05:55:28 +0900

    fix: status code 오타 수정

--- src/back-end/web/applies/exceptions.py
+++ src/back-end/web/applies/exceptions.py
@@ -1,11 +1,18 @@
 """Exceptions related to applies"""
-from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
 class ApplyAlreadyExistException(APIException):
     """Apply conflicts"""
 
-    status_code = status.HTTP_409_CONFLICT
+    status_code = 409
     default_detail = "이미 신청한 모임입니다."
     default_code = "group_conflicts"
+
+
+class ApplyNotFoundException(APIException):
+    """Apply conflicts"""
+
+    status_code = 404
+    default_detail = "모임 신청 기록을 찾을 수 없습니다."
+    default_code = "apply_not_found"
