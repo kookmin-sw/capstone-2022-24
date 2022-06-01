0a86a3926fc8c9b847626815636c318a8bd81da3
Dahee Goo <dadaahee@gmail.com>
2022-06-01 22:27:08 +0900

    update: beat(scheduler) / worker 분리, 테스트중

--- src/back-end/web/config/tasks.py
+++ src/back-end/web/config/tasks.py
@@ -1,7 +1,7 @@
 """Celery worker & scheduler settings"""
 from __future__ import absolute_import, unicode_literals
 
-from celery import Celery
+from celery import Celery, shared_task
 from django.conf import settings
 
 # is it needed?
@@ -12,13 +12,31 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # Celery beat settings
+app.conf.timezone = "Asia/Seoul"
 app.conf.beat_schedule = {
     # periodic events like this
-    # "periodic_add_numbers": {
-    #     "task": "my_app.tasks.add_numbers",
-    #     "schedule": crontab(minute="*\1"),
-    # },
+    "periodic_add_numbers": {
+        "task": "config.tasks.add_numbers",
+        # "schedule": crontab(),
+        "schedule": 5.0,
+        "args": (1, 1),
+    },
 }
+
+
+@shared_task
+def add_numbers(a, b):
+    """(sample) add numbers"""
+    print("add_numbers are called:", a + b)
+    print("hi!!!")
+    return a + b
+
+
+@shared_task
+def sub_numbers(a, b):
+    """(sample) subtract numbers"""
+    print("sub numbers: a-b", a - b)
+    return a - b
 
 
 @app.task(bind=True)
