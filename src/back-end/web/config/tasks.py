c7e6ed9a27ac77affd4b7e41a0e7a2a4bee70ba8
Dahee Goo <dadaahee@gmail.com>
2022-06-02 01:33:47 +0900

    chore: 샘플 비트 남기고 코드 정리

--- src/back-end/web/config/tasks.py
+++ src/back-end/web/config/tasks.py
@@ -1,11 +1,8 @@
 """Celery worker & scheduler settings"""
 from __future__ import absolute_import, unicode_literals
 
-from celery import Celery, shared_task
+from celery import Celery
 from django.conf import settings
-
-# is it needed?
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -15,31 +12,9 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.timezone = "Asia/Seoul"
 app.conf.beat_schedule = {
     # periodic events like this
-    "periodic_add_numbers": {
-        "task": "config.tasks.add_numbers",
-        # "schedule": crontab(),
-        "schedule": 5.0,
-        "args": (1, 1),
-    },
+    # "periodic_add_numbers": {
+    #     "task": "config.tasks.add_numbers",
+    #     "schedule": crontab(),
+    #     "args": (1, 1),
+    # },
 }
-
-
-@shared_task
-def add_numbers(a, b):
-    """(sample) add numbers"""
-    print("add_numbers are called:", a + b)
-    print("hi!!!")
-    return a + b
-
-
-@shared_task
-def sub_numbers(a, b):
-    """(sample) subtract numbers"""
-    print("sub numbers: a-b", a - b)
-    return a - b
-
-
-@app.task(bind=True)
-def debug_task(self):
-    """(sample) Debug tasks"""
-    print(f"Request: {self.request!r}")
